6b73cd81bfbb3e287d3557e4cf5b2dc0780551ef
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-24 21:28:00 -0300

    Escreva um programa C# Sharp para imprimir a soma de dois números

---
+++ 01-Basic/Exercice02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Escreva um programa C# Sharp para imprimir a soma de dois números.
+
+namespace Exercice02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 5, b = 7;
+
+            System.Console.WriteLine($" a soma entre {a} e {b} é {a + b}.");
+        }
+    }
+}
