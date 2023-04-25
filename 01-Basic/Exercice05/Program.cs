95ec4f95dbdf0c4de72f96fcd9bcc69d1364401c
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-25 13:54:55 -0300

    Escreva um programa C# Sharp para trocar dois números

---
+++ 01-Basic/Exercice05/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+Escreva um programa C# Sharp para trocar dois números.
+
+Dados de teste:
+Insira o primeiro número: 5
+Insira o segundo número: 6
+
+Saída esperada após a troca:
+Primeiro número: 6
+Segundo número: 5
+*/
+
+namespace Exercice05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            System.Console.Write("Insira o primeiro número: ");
+            string a = Console.ReadLine();
+
+            System.Console.Write("Insira o segundo número: ");
+            string b = Console.ReadLine();
+
+            if (int.TryParse(a, out int c) && int.TryParse(b, out int d))
+            {
+                int temporary = c;
+                c = d;
+                d = temporary;
+
+                System.Console.WriteLine($"Primeiro número: {c}");
+                System.Console.WriteLine($"Segundo número: {d}");
+            }
+            else
+            {
+                System.Console.WriteLine("Entrada inválida. Por favor digite um número inteiro.");
+            }
+        }
+    }
+}
