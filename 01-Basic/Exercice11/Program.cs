1f9ab89de81ff38c2e82e1752952a3e269af257e
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-25 17:37:09 -0300

    Escreva um programa C# Sharp que receba uma idade (por exemplo, 20) como entrada e imprima algo como 'Você parece ter mais de 20 anos'

---
+++ 01-Basic/Exercice11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+Escreva um programa C# Sharp que receba uma idade (por exemplo, 20) como entrada e imprima algo como "Você parece ter mais de 20 anos".
+
+Dados do teste:
+Digite sua idade - 25
+Resultado esperado:
+Você parece ter mais de 25 anos
+*/
+
+namespace Exercice11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite sua idade: ");
+            string age = Console.ReadLine();
+
+            if (int.TryParse(age, out int ageInt))
+            {
+                System.Console.WriteLine($"Você parece ter mais de {ageInt} anos.");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Entrada de dados incorreta. Insira apenas números inteiros.");
+            }
+        }
+    }
+}
