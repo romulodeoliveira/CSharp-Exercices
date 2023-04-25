852533cdb7793cf09753d5f3e9195968960bf850
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-24 21:30:32 -0300

    Escreva um programa C# Sharp para imprimir o resultado da divisão de dois números

---
+++ 01-Basic/Exercice03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Escreva um programa C# Sharp para imprimir o resultado da divisão de dois números.
+
+namespace Exercice03
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10, b = 2;
+
+            System.Console.WriteLine($"A divisão entre {a} e {b} é {a / b}");
+        }
+    }
+}
