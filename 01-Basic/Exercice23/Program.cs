71f2dcc9b90ad85ade8cfa4984dc76bd79258d5a
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-26 19:47:38 -0300

    Escreva um programa C# para converter uma determinada string em letras minúsculas.

---
+++ 01-Basic/Exercice23/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+Escreva um programa C# para converter uma determinada string em letras minúsculas.
+
+Saída de amostra:
+escreva um programa nítido para exibir o seguinte padrão usando o alfabeto.
+*/
+
+namespace Exercice23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "O rato roeu a roupa do rei de Roma.";
+
+            System.Console.WriteLine(str.ToLower());
+        }
+    }
+}
