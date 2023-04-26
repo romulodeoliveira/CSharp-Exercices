ebe15cd095b612795b301e040b1ca8c6b965f640
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-26 19:43:30 -0300

    Escreva um programa C# para verificar se um determinado número inteiro está dentro de 20 de 100 ou 200.

---
+++ 01-Basic/Exercice22/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/*
+Escreva um programa C# para verificar se um determinado número inteiro está dentro de 20 de 100 ou 200.
+*/
+
+namespace Exercice22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Insira um número: ");
+            string str = Console.ReadLine();
+
+            if (int.TryParse(str, out int num))
+            {
+                if (num <= 20)
+                {
+                    System.Console.WriteLine("Está dentro de 20!");
+                }
+
+                else if (num <= 100)
+                {
+                    System.Console.WriteLine("Está dentro de 100!");
+                }
+
+                else if (num <= 200)
+                {
+                    System.Console.WriteLine("Está dentro de 200!");
+                }
+
+                else
+                {
+                    if (num < 200)
+                    {
+                        System.Console.WriteLine("É maior que 200!");
+                    }
+
+                    else
+                    {
+                        System.Console.WriteLine("É negativo!");
+                    }
+                }
+            }
+
+            else
+            {
+                System.Console.WriteLine("Entrada de dados incorreta. Insira apenas números inteiros");
+            }
+        }
+    }
+}
