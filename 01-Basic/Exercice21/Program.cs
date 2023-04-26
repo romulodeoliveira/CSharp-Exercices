8862ec3a849c6d3c66f9a5ca7a4d348d8f8e9293
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-26 19:26:15 -0300

    Escreva um programa em C# para verificar a soma dos dois inteiros fornecidos e retorne true se um dos inteiros for 20 ou se a soma for 20

---
+++ 01-Basic/Exercice21/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+/*
+Escreva um programa em C# para verificar a soma dos dois inteiros fornecidos e retorne true se um dos inteiros for 20 ou se a soma for 20.
+*/
+
+namespace Exercice21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Insira o primeiro número: ");
+            string v1 = Console.ReadLine();
+
+            System.Console.Write("Insira o segundo número: ");
+            string v2 = Console.ReadLine();
+
+            if (int.TryParse(v1, out int n1) && int.TryParse(v2, out int n2))
+            {
+                if (n1 == 20 || n2 == 20 || n1 + n2 == 20)
+                {
+                    System.Console.WriteLine("Verdadeiro.");
+                }
+
+                else
+                {
+                    System.Console.WriteLine("Falso");
+                }
+            }
+
+            else
+            {
+                System.Console.WriteLine("Entrada de dados incorreta. Insira apenas números inteiros.");
+            }
+        }
+    }
+}
