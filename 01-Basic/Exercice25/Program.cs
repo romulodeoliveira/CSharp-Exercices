92f6a9c9ec3d685cc4f6ddd63652a77beb785013
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-26 20:23:52 -0300

    Escreva um programa C# para imprimir os números ímpares de 1 a 99. Imprime um número por linha.

---
+++ 01-Basic/Exercice25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+Escreva um programa C# para imprimir os números ímpares de 1 a 99. Imprime um número por linha.
+
+Exemplo de Saída:
+Números ímpares de 1 a 99. Imprime um número por linha.
+1
+3
+5
+7
+9
+...
+95
+97
+99
+*/
+
+namespace Exercice25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
