86e63bea3d8f4191068e086b06e216c49c64dca1
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-26 04:02:31 -0300

    Escreva um programa C# para criar uma nova string a partir de uma determinada string em que o primeiro e o último caracteres mudarão de posição

---
+++ 01-Basic/Exercice16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+Escreva um programa C# para criar uma nova string a partir de uma determinada string em que o primeiro e o último caracteres mudarão de posição.
+
+Dados de teste:
+w3resource
+Python
+Exemplo de saída:
+e3resourcew
+nythoP
+x
+*/
+
+namespace Exercice16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "CSHARP";
+            string str2 = str1.Substring(5, 1) + str1.Substring(1, 4) + str1.Substring(0, 1);
+
+            System.Console.WriteLine(str2);
+        }
+    }
+}
