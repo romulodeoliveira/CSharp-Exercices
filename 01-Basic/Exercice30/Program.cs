e49633bdd5861a6137068f7070e7f7635f861bed
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-27 20:46:02 -0300

    Escreva um programa C# para converter um número hexadecimal em decimal

---
+++ 01-Basic/Exercice30/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+Escreva um programa C# para converter um número hexadecimal em decimal.
+
+Exemplo de saída:
+Número hexadecimal: 4B0
+Converter para
+número decimal: 1200
+*/
+
+namespace Exercice30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string hexNumber = "1A82";
+            int decimalNumber = Convert.ToInt32(hexNumber, 16);
+            System.Console.WriteLine(decimalNumber);
+        }
+    }
+}
