fb31240822b892c231d3c28baf7759e0ba9edfb1
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-27 20:28:28 -0300

    Escreva um programa C# para encontrar o tamanho de um arquivo especificado em bytes

---
+++ 01-Basic/Exercice29/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+Escreva um programa C# para encontrar o tamanho de um arquivo especificado em bytes.
+Exemplo de Saída:
+Tamanho de um arquivo: 31
+*/
+
+namespace Exercice29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "01-Basic/Exercice29/file.txt";
+            FileInfo fileInfo = new FileInfo(fileName);
+            double lengthFile = fileInfo.Length;
+
+            System.Console.WriteLine($"Tamanho do arquivo: {lengthFile} bytes.");
+        }
+    }
+}
