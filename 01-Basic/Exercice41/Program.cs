c8734871bd7e5790300270dd1618da24cd3071f0
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-01 20:19:09 -0300

    Escreva um programa C# para verificar se uma determinada string contém o caractere 'w' entre 1 e 3 vezes.

---
+++ 01-Basic/Exercice41/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+Escreva um programa C# para verificar se uma determinada string contém o caractere 'w' entre 1 e 3 vezes.
+
+Dados de teste:
+Insira uma string (contém pelo menos um caractere 'w'): w3resource
+Teste se a string contém o caractere 'w' entre 1 e 3 vezes:
+Exemplo de saída
+Verdadeiro
+*/
+
+namespace Exercice41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite uma frase: ");
+            string str = Console.ReadLine();
+            int quantity = str.Count(a => a == 'w');
+
+            if (quantity > 0 && quantity <= 3)
+            {
+                System.Console.WriteLine("Verdadeiro");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Falso");
+            }
+        }
+    }
+}
