cd2aaef56ee092d13c3810e7c3f01f47953a35ac
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-01 03:42:19 -0300

    Escreva um programa C# para obter uma nova string de dois caracteres de uma determinada string. O primeiro e o segundo caractere da string fornecida devem ser 'P' e 'H', então o PHP será 'PH'.

---
+++ 01-Basic/Exercice38/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+Escreva um programa C# para obter uma nova string de dois caracteres de uma determinada string. O primeiro e o segundo caractere da string fornecida devem ser "P" e "H", então o PHP será "PH".
+
+Dados de teste: PHP
+Exemplo de saída:
+PH
+*/
+
+namespace Exercice38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite uma frase: ");
+            string str = Console.ReadLine();
+
+            string newStr = str.Substring(0, 2);
+
+            System.Console.WriteLine(newStr);
+        }
+    }
+}
