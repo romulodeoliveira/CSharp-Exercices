a581105bc4ce0887c936b450b4e91287a69bef80
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-01 00:41:30 -0300

    Escreva um programa C# para verificar se HP aparece na segunda posição em uma string e retorna a string sem HP.

---
+++ 01-Basic/Exercice37/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+Escreva um programa C# para verificar se "HP" aparece na segunda posição em uma string e retorna a string sem "HP".
+
+Test Data: PHP Tutorial
+Sample Output:
+P Tutorial
+*/
+
+namespace Exercice36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite uma frase: ");
+            string str = Console.ReadLine();
+
+            string newStr = str.Replace("HP", "");
+
+            System.Console.WriteLine(newStr);
+        }
+    }
+}
