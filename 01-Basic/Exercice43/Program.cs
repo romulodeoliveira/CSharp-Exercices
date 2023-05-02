67ffdc6553b2aed903f8af2844e2e54817d2d6e9
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-01 22:00:55 -0300

    Escreva um programa C# para verificar se uma determinada string começa com w e imediatamente seguida por dois ww.

---
+++ 01-Basic/Exercice43/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/*
+Escreva um programa C# para verificar se uma determinada string começa com "w" e imediatamente seguida por dois "ww".
+Dados de teste:
+Insira uma string: www
+Sample Output
+False
+*/
+
+namespace Exercice43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite uma frase: ");
+            string str = Console.ReadLine();
+            string test = "w";
+
+            if (str[0] == 'w')
+            {
+                if (str[1] == 'w' && str[2] == 'w')
+                {
+                    System.Console.WriteLine("Verdadeiro");
+                }
+
+                else
+                {
+                    System.Console.WriteLine("Falso");
+                }
+            }
+
+            else
+            {
+                System.Console.WriteLine("Falso");
+            }
+        }
+    }
+}
+
+/*
+Em C#, aspas simples (') são usadas para definir um caractere (por exemplo, 'a') e aspas duplas (") são usadas para definir uma string (por exemplo, "Olá").
+*/
