4c3893c0960c20692a05bfb15d0340c9a65c5493
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-02 18:46:58 -0300

    Escreva um programa C# para criar uma nova string de todos os outros caracteres (posição ímpar) a partir da primeira posição de uma determinada string.

---
+++ 01-Basic/Exercice44/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+Escreva um programa C# para criar uma nova string de todos os outros caracteres (posição ímpar) a partir da primeira posição de uma determinada string.
+
+Dados de teste:
+Insira uma string: w3resource
+Exemplo de saída
+wrsuc
+*/
+
+namespace Exercice44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite uma frase: ");
+            string str = Console.ReadLine();
+
+            string newStr = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    newStr += str[i];
+                }
+            }
+
+            System.Console.WriteLine(newStr);
+        }
+    }
+}
