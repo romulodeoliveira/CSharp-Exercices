3915594a7d1e9cfecc104f8d77e884b6bc477e05
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-03 20:06:06 -0300

    Escreva um programa C# para obter o século de um ano.

---
+++ 01-Basic/Exercice54/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Escreva um programa C# para obter o século de um ano.
+
+namespace Exercice54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = DateTime.Now.Year;
+
+            int seculo = (int)Math.Ceiling((double)year / 100);
+
+            Console.WriteLine($"O século do ano {year} é o {seculo}º século.");
+        }
+    }
+}
