97344efae173a191d0e27216a36781c8556008d1
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-03 15:07:22 -0300

    Escreva um programa em C# para calcular a soma de todos os elementos de um array de inteiros.

---
+++ 01-Basic/Exercice47/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+Escreva um programa em C# para calcular a soma de todos os elementos de um array de inteiros.
+
+Test Data:
+Array1: [1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1]
+Sample Output
+Sum: 69
+*/
+
+namespace Exercice47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+            int sum = 0;
+            int i = 0;
+
+            while (i < nums.Length)
+            {
+                sum = sum + nums[i];
+
+                i++;
+            }
+
+            System.Console.WriteLine(sum);
+        }
+    }
+}
