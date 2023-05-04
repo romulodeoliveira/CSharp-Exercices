202d5a869e2bab199875bf5eb8f362b8f12251b4
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-04 20:31:11 -0300

    Escreva um programa C# para encontrar o par de elementos adjacentes que tem o maior produto de uma determinada matriz que é igual a um determinado valor.

---
+++ 01-Basic/Exercice55/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*
+Escreva um programa C# para encontrar o par de elementos adjacentes que tem o maior produto de uma determinada matriz que é igual a um determinado valor.
+*/
+
+namespace Exercice55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 3, 4, 5, 6 };
+
+            int highestValue = int.MinValue;
+
+            int i = 0;
+            int j = 1;
+
+            while (j < nums.Length)
+            {
+                if ((nums[i] * nums[j]) > highestValue)
+                {
+                    highestValue = nums[i] * nums[j];
+                }
+                i++;
+                j++;
+            }
+
+            System.Console.WriteLine(highestValue);
+        }
+    }
+}
