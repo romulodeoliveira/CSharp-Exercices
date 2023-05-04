976edb97aa188bc992bbc6248b77a1857b86355d
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-04 00:18:00 -0300

    Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa, conforme exemplos.

---
+++ 18-NelioAlvesC#/Seção03/Exercice01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa, conforme exemplos.
+*/
+
+namespace Exercice01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Digite o primeiro número: ");
+            string firstVariable = Console.ReadLine();
+
+            System.Console.WriteLine("Digite o segundo número: ");
+            string secondVariable = Console.ReadLine();
+
+            if (int.TryParse(firstVariable, out int firstNumber) && int.TryParse(secondVariable, out int secondNumber))
+            {
+                System.Console.WriteLine($"Você digitou {firstNumber} e {secondNumber}");
+                System.Console.WriteLine($"A soma dos dois é {firstNumber + secondNumber}");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+            }
+        }
+    }
+}
