4b8d78516762df33fa47dcbea7689e6e5f34ba87
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-04 21:36:52 -0300

    Escreva um programa C# para verificar se uma determinada string é um palíndromo ou não.

---
+++ 01-Basic/Exercice56/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/*
+Escreva um programa C# para verificar se uma determinada string é um palíndromo ou não.
+Exemplo de amostra:
+Para 'aaa' a saída deve ser true
+Para 'abcd' a saída deve ser false
+*/
+
+namespace Exercice56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite uma frase: ");
+            string str = Console.ReadLine();
+
+            char[] chars = str.ToCharArray();
+            char[] reversedChars = new char[chars.Length];
+
+            int i = str.Length - 1;
+            int j = 0;
+
+            while (i >= 0)
+            {
+                reversedChars[j] = chars[i];
+
+                i--;
+                j++;
+            }
+
+            string reversedStr = new string(reversedChars);
+
+            if (str == reversedStr)
+            {
+                System.Console.WriteLine($"Verdadeiro: {reversedStr}");
+            }
+
+            else
+            {
+                System.Console.WriteLine($"Falso: {reversedStr}");
+            }
+        }
+    }
+}
