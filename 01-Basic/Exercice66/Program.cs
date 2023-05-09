7cb15a77684a3263fa4c2a076ca1c2f4f8afc054
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-09 06:17:17 -0300

    Escreva um programa C# Sharp para encontrar o valor mínimo de dois números dados, representados como string.

---
+++ 01-Basic/Exercice66/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Escreva um programa C# Sharp para encontrar o valor mínimo de dois números dados, representados como string.
+
+namespace Exercice65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite o primeiro número: ");
+            string str1 = Console.ReadLine();
+
+            System.Console.Write("Digite o segundo número: ");
+            string str2 = Console.ReadLine();
+
+            if (int.TryParse(str1, out int num1) && int.TryParse(str2, out int num2))
+            {
+                System.Console.WriteLine($"Menor valor: {Math.Min(num1, num2)}");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Entrada de dados incorreta. Insira apenas numeros inteiros.");
+            }
+        }
+    }
+}
