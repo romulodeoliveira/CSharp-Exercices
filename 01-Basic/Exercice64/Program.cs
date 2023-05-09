b4e6c46a322c77050c092cce7492013bd669f5e7
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-09 05:43:24 -0300

    Escreva um programa C# Sharp para obter o nome do arquivo (incluindo a extensão) de um determinado caminho.

---
+++ 01-Basic/Exercice64/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// Escreva um programa C# Sharp para obter o nome do arquivo (incluindo a extensão) de um determinado caminho.
+
+namespace Exercice64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "./01-Basic/Exercice64/HelloWorld.cs";
+            string extension = Path.GetExtension(path);
+
+            System.Console.WriteLine($"A extensão é: {extension}");
+        }
+    }
+}
