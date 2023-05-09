23957eb9a638697b17053df43c5c542ee2aa2c97
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-09 05:33:58 -0300

    Escreva um programa C# para verificar se um determinado número está presente em uma matriz de números.

---
+++ 01-Basic/Exercice63/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// Escreva um programa C# para verificar se um determinado número está presente em uma matriz de números.
+
+namespace Exercice63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int[] nums = new int[] { 1, 3, 5, 7, 9 };
+            bool isPresent = false;
+            int i = 0;
+
+            while (i < nums.Length)
+            {
+                if (num == nums[i])
+                {
+                    isPresent = true;
+                    break;
+                }
+
+                i++;
+            }
+
+            if (isPresent)
+            {
+                System.Console.WriteLine("Está presente");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Não está presente");
+            }
+        }
+    }
+}
