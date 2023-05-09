51348480533f672f087594a912939a69bc482ca3
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-09 06:08:14 -0300

    Escreva um programa C# Sharp para multiplicar todos os elementos de uma determinada matriz de números pelo comprimento da matriz.

---
+++ 01-Basic/Exercice65/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// Escreva um programa C# Sharp para multiplicar todos os elementos de uma determinada matriz de números pelo comprimento da matriz.
+
+namespace Exercice65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 4, 6, 8 };
+
+            int i = 0;
+            while (i < nums.Length)
+            {
+                System.Console.Write($"| {nums[i] * nums.Length} ");
+                i++;
+            }
+            System.Console.Write("|");
+            System.Console.WriteLine();
+        }
+    }
+}
