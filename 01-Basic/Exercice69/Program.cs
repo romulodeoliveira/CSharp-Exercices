c84684fc7b57fdd737353095a86ddd48e3c01c6a
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-10 22:37:22 -0300

    Escreva um programa C# Sharp para verificar se uma determinada string contém apenas caracteres minúsculos ou maiúsculos.

---
+++ 01-Basic/Exercice69/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// Escreva um programa C# Sharp para verificar se uma determinada string contém apenas caracteres minúsculos ou maiúsculos.
+
+namespace Exercice69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite uma frase: ");
+            string str = Console.ReadLine();
+
+            string str1 = str.Replace(" ", "");
+
+            bool containOnlyUpperCase = str1.All(char.IsUpper);
+            bool containOnlyLowerCase = str1.All(char.IsLower);
+
+            if (containOnlyUpperCase || containOnlyLowerCase)
+            {
+                System.Console.WriteLine("Contém apenas letras maiusculas ou minusculas.");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Contem os dois.");
+            }
+        }
+    }
+}
