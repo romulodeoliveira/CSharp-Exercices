cf969eff21dee6ac0bb39fc718df09e6298bcb4b
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-11 20:38:21 -0300

    Escreva um programa C# Sharp para remover o primeiro e o último elemento de uma determinada string.

---
+++ 01-Basic/Exercice70/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+Escreva um programa C# Sharp para remover o primeiro e o último elemento de uma determinada string. 
+
+Exemplo de saída:
+String original: PHP
+Depois de remover o primeiro e o último elemento: H
+String original: Python
+Depois de remover o primeiro e o último elemento: ytho
+String original: JavaScript
+Depois de remover o primeiro e o último elemento: avaScrip
+*/
+
+namespace Exercice70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Ola Mundo";
+
+            string str1 = str.Substring(1, str.Length - 2);
+
+            Console.WriteLine("String original: " + str);
+            Console.WriteLine("Depois de remover o primeiro e o último elemento: " + str1);
+        }
+    }
+}
