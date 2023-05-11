874aeaa7cef9a89ddb6fbdc437f7d7738479bea6
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-10 22:23:41 -0300

    Escreva um programa C# Sharp para contar um caractere especificado (ambos os casos) em uma determinada string.

---
+++ 01-Basic/Exercice68/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Escreva um programa C# Sharp para contar um caractere especificado (ambos os casos) em uma determinada string.
+
+namespace Exercice68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Olá, Mundo!";
+            int count = str.Count(c => c == 'o');
+            System.Console.WriteLine(count);
+        }
+    }
+}
