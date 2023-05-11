0b411da85a5b842cfdc48723c3e80722deb94965
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-11 20:55:30 -0300

    Escreva um programa C# Sharp para obter a diferença absoluta entre n e 51. Se n for maior que 51, retorne o triplo da diferença absoluta.

---
+++ 02-BasicAlgorithm/Exercice02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+Escreva um programa C# Sharp para obter a diferença absoluta entre n e 51. Se n for maior que 51, retorne o triplo da diferença absoluta.
+
+Entrada de amostra :
+53
+30
+51
+
+Saída esperada :
+6
+21
+0
+*/
+
+namespace Exercice02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Insira um número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine(Math.Abs(num - 51));
+        }
+    }
+}
