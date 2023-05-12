e00c91f5359434c72fe2d5b199d902de884ee38b
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-11 21:17:48 -0300

    Escreva um programa C# Sharp para criar uma nova string onde 'if' é adicionado à frente de uma determinada string. Se a string já começar com 'if', retorne a string inalterada.

---
+++ 02-BasicAlgorithm/Exercice05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+Escreva um programa C# Sharp para criar uma nova string onde 'if' é adicionado à frente de uma determinada string. Se a string já começar com 'if', retorne a string inalterada.
+
+Exemplo de entrada :
+"if else"
+"else"
+
+Saída esperada :
+se mais
+se mais
+*/
+
+namespace Exercice05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "a vida é uma droga";
+            string[] strArray = str.Split(" ");
+
+            if (strArray[0] != "se")
+            {
+                System.Console.WriteLine($"se {str}");
+            }
+        }
+    }
+}
