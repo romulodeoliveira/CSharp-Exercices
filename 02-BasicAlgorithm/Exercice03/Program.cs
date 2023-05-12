7c7d22daafeea1c7a8d6291b17dc9416f0d946e7
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-11 21:03:25 -0300

    Escreva um programa C# Sharp para verificar dois números inteiros fornecidos e retorne true se um deles for 30 ou se a soma for 30.

---
+++ 02-BasicAlgorithm/Exercice03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/*
+Escreva um programa C# Sharp para verificar dois números inteiros fornecidos e retorne true se um deles for 30 ou se a soma for 30.
+
+Entrada de amostra :
+30, 0
+25, 5
+20, 30
+20, 25
+
+Saída esperada :
+Verdadeiro
+Verdadeiro
+Verdadeiro
+Falso
+*/
+
+namespace Exercice03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Insira o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            System.Console.Write("Insira o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 + num2 == 30 || num1 == 30 || num2 == 30)
+            {
+                System.Console.WriteLine("Verdadeiro");
+            }
+
+            else
+            {
+                System.Console.WriteLine("False");
+            }
+        }
+    }
+}
