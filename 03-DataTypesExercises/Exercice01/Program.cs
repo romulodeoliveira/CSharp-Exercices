8eb73063b622d55fdc93b757f1224451e40f281b
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-20 18:48:13 -0300

    Escreva um programa C# Sharp que receba três letras como entrada e as exiba na ordem inversa.

---
+++ 03-DataTypesExercises/Exercice01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+Escreva um programa C# Sharp que receba três letras como entrada e as exiba na ordem inversa.
+
+Test Data
+Digite a letra: b
+Digite a letra: a
+Digite a letra: t
+Saída esperada :
+tab
+*/
+
+namespace Exercice01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = new string[] { "pão", "quejo", "presunto" };
+
+            for (int i = values.Length - 1; i > -1; i--)
+            {
+                System.Console.WriteLine(values[i]);
+            }
+        }
+    }
+}
