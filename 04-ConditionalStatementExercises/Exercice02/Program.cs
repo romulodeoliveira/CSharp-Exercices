9a1669c64c10bf490265321da58562260e024a9e
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-21 17:18:20 -0300

    Escreva um programa C# Sharp para verificar se um determinado número é par ou ímpar.

---
+++ 04-ConditionalStatementExercises/Exercice02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+Escreva um programa C# Sharp para verificar se um determinado número é par ou ímpar.
+
+Dados de teste: 15
+
+Saída esperada :
+15 é um inteiro ímpar
+*/
+
+namespace Exercice02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 11;
+
+            if (a % 2 == 0)
+            {
+                System.Console.WriteLine("Par");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Impar");
+            }
+        }
+    }
+}
