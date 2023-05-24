51cbcf3e4b3a796d042bf20b331f69c344a28a96
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-23 21:17:29 -0300

    Escreva um programa C# Sharp para verificar se um determinado número é positivo ou negativo.

---
+++ 04-ConditionalStatementExercises/Exercice03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+Escreva um programa C# Sharp para verificar se um determinado número é positivo ou negativo.
+
+Dados de teste: 14
+
+Saída esperada :
+14 é um número positivo
+*/
+
+namespace Exercice03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            if (num >= 0)
+            {
+                System.Console.WriteLine("Positivo");
+            }
+            else
+            {
+                System.Console.WriteLine("Negativo");
+            }
+        }
+    }
+}
