9cb3de7118f2c0b3fddca76a1f3fff0bb38099bc
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-24 16:33:16 -0300

    Escreva um programa C# Sharp para descobrir se um determinado ano é bissexto ou não.

---
+++ 04-ConditionalStatementExercises/Exercice04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+Escreva um programa C# Sharp para descobrir se um determinado ano é bissexto ou não.
+
+Dados de teste: 2016
+
+Saída esperada :
+2016 é um ano bissexto.
+*/
+
+namespace Exercice04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2002;
+
+            if (year % 4 == 0)
+            {
+                System.Console.WriteLine("Bissexto.");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Não bissexto.");
+            }
+        }
+    }
+}
