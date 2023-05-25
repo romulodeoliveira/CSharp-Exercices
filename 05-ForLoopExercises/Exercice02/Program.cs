67182759d157ffda4c8b9504ffbf39ff5be4c72a
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-25 19:16:46 -0300

    Escreva um programa C# para encontrar a soma dos primeiros 10 números naturais.

---
+++ 05-ForLoopExercises/Exercice02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+Escreva um programa C# para encontrar a soma dos primeiros 10 números naturais.
+Resultado Esperado :
+Os primeiros 10 números naturais são:
+1 2 3 4 5 6 7 8 9 10
+A soma é: 55
+*/
+
+namespace Exercice02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            for (int i = 1; i < 11; i++)
+            {
+                System.Console.Write($"{i} ");
+                total += i;
+            }
+            System.Console.WriteLine($"\nA soma é: {total}");
+        }
+    }
+}
