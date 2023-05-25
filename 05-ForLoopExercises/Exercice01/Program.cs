72d7c0c1271ce4a2ad57ffb11ac4a041f9d4c735
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-25 19:00:46 -0300

    Escreva um programa em C# para exibir os 10 primeiros números naturais.

---
+++ 05-ForLoopExercises/Exercice01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+Escreva um programa em C# Sharp para exibir os 10 primeiros números naturais.
+Resultado Esperado :
+1 2 3 4 5 6 7 8 9 10
+*/
+
+namespace Exercice01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                System.Console.Write($"{i} ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
+
