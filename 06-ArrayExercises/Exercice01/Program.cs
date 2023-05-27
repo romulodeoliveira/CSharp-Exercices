f54faa463050858b9a3185887dd092e3cd1f98f5
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-26 22:34:36 -0300

    Escreva um programa C# que armazene elementos em um array e os imprima.

---
+++ 06-ArrayExercises/Exercice01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+Escreva um programa C# que armazene elementos em um array e os imprima.
+Dados de teste:
+Insira 10 elementos na matriz:
+elemento - 0: 1
+elemento - 1: 1
+elemento - 2: 2
+.......
+Saída esperada :
+Os elementos na matriz são: 1 1 2 3 4 5 6 7 8 9
+*/
+
+namespace Exercice01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = new string[] { "banana", "orange", "apple", "lemon", "avocados" };
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                System.Console.WriteLine($"Elemento - {i}: {fruits[i]}");
+            }
+        }
+    }
+}
