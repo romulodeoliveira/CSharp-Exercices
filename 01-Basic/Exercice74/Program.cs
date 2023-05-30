ded373aae65f66b524e93aff3f01a9a77ac2b0b9
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-30 14:15:42 -0300

    Escreva um programa C# para verificar se o comprimento de uma determinada string é par ou ímpar.

---
+++ 01-Basic/Exercice74/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+Escreva um programa C# para verificar se o comprimento de uma determinada string é par ou ímpar. Retorne 'Comprimento ímpar' se o comprimento da string for ímpar, caso contrário, 'Comprimento par'.
+
+Exemplo de saída:
+
+String original: PHP
+Converter as letras da string em ordem alfabética: Comprimento ímpar
+
+String original: javascript
+Converter as letras da string em ordem alfabética: Comprimento par
+
+String original: python
+Converter as letras do dita string em ordem alfabética: Mesmo comprimento
+*/
+
+namespace Exercice74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "teste";
+
+            System.Console.WriteLine(IsOddOrEven.LengthOddOrEven(str1));
+        }
+    }
+}
