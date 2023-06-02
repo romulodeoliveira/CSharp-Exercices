13e01579e1bc8e9a0fb2f75c9f247e7313a929c9
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-02 00:06:02 -0300

    Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.

---
+++ 18-NelioAlvesC#/Seção03/Exercice07/IsPositive.cs
@@ -0,0 +1,17 @@
+namespace Exercice07
+{
+    public class IsPositive
+    {
+        public static bool Number(int n)
+        {
+            if (n >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
