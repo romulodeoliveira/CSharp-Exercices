6634e84a7fba0371a4aabe03a24420255bc17287
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-02 00:15:59 -0300

    Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.

---
+++ 18-NelioAlvesC#/Seção03/Exercice08/IsEven.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercice08
+{
+    public class IsEven
+    {
+        public static bool Number(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
