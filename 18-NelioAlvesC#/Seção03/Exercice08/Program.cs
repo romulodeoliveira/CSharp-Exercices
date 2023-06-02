89dd751e629825c8e1be73d8c572966df3fcc16d
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-02 00:15:59 -0300

    Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.

---
+++ 18-NelioAlvesC#/Seção03/Exercice08/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/*
+Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
+Exemplos:
+
+Entrada:
+12
+
+Saída:
+PAR
+
+Entrada:
+-27
+
+Saída:
+IMPAR
+
+Entrada:
+0
+
+Saída:
+PAR
+*/
+
+namespace Exercice08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite um número: ");
+            string str = Console.ReadLine();
+
+            if (int.TryParse(str, out int num))
+            {
+                if (IsEven.Number(num))
+                {
+                    System.Console.WriteLine("Par");
+                }
+                else
+                {
+                    System.Console.WriteLine("Impar");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Insira um número inteiro.");
+            }
+        }
+    }
+}
