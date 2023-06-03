cbfb0638849f2343c6c3308eccda20bf33e263fc
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-03 00:00:02 -0300

    resolução de exercicio

---
+++ 18-NelioAlvesC#/Seção03/Exercice15/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+
+Exemplo:
+
+Entrada:
+2200
+
+Saída:
+Senha Invalida
+
+Entrada:
+1020
+
+Saída:
+Senha Invalida
+
+Entrada:
+2022
+
+Saída:
+Senha Invalida
+
+Entrada:
+2002
+
+Saída:
+Acesso Permitido
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int senhaCorreta = 2002;
+        int senhaDigitada;
+
+        do
+        {
+            Console.WriteLine("Digite a senha:");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out senhaDigitada))
+            {
+                if (senhaDigitada == senhaCorreta)
+                {
+                    Console.WriteLine("Acesso Permitido");
+                    break; // Encerra o loop quando a senha é correta
+                }
+                else
+                {
+                    Console.WriteLine("Senha Inválida");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Senha Inválida");
+            }
+        } while (true);
+
+        Console.ReadLine();
+    }
+}
