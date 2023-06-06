a063c74a0e4d27593ef8e1f640ee7971dfc4c965
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-06 17:08:03 -0300

    Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.

---
+++ 18-NelioAlvesC#/Seção03/Exercice18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/*
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+
+Exemplo:
+
+Entrada:
+8
+
+Saída:
+1
+3
+5
+7
+*/
+
+namespace Exercice18 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
