246935cb20621718375cae19b35f7b031b17bdeb
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-13 23:01:08 -0300

    Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.

---
+++ 18-NelioAlvesC#/Seção04/Exercice01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.
+
+Exemplo:
+
+Dados da primeira pessoa:
+Nome: Maria
+Idade: 17
+
+Dados da segunda pessoa:
+Nome: Joao
+Idade: 16
+
+Pessoa mais velha: Maria
+*/
+
+namespace Exercice01;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Form person1 = new Form("Joazinho", 12);
+        Form person2 = new Form("Pedrinho", 13);
+
+        Form.AverageAge(person1, person2);
+    }
+}
