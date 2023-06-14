e56d7f759a81f92441e434090c75ddb7e4ca52c7
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-13 23:21:34 -0300

    Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários.

---
+++ 18-NelioAlvesC#/Seção04/Exercice02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários.
+
+Exemplo:
+
+Dados do primeiro funcionário:
+Nome: Carlos Silva
+Salário: 6300.00
+
+Dados do segundo funcionário:
+Nome: Ana Marques
+Salário: 6700.00
+
+Salário médio = 6500.00
+*/
+
+namespace Exercice02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Form employee1 = new Form("Joaozinho", 6300.00);
+        Form employee2 = new Form("Pedrinho", 6700.00);
+
+        Console.WriteLine($"A média salarial é de R$ {Form.AverageSalary(employee1, employee2):F2}");
+    }
+}
