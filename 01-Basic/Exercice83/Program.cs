06faa2b4afc582439e66d0e2efe65bf78599ab31
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-21 10:34:44 -0300

    resolução do exercicio 83

--- 01-Basic/Exercice83/Program.cs
+++ 01-Basic/Exercice83/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para remover todas as vogais de uma determinada string.
 
 Exemplo de saída:
@@ -9,3 +11,26 @@ Depois de remover todas as vogais da string: C Shrp
 String original: JavaScript
 Depois de remover todas as vogais da string dita string: JvScrpt
 */
+
+namespace Exercice83;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite uma frase: ");
+        string input = Console.ReadLine();
+        string result = "";
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] != 'a' && input[i] != 'e' && input[i] != 'i' && input[i] != 'o' && input[i] != 'u')
+            {
+                result += input[i];
+            }
+        }
+
+        Console.WriteLine($"Str1 = {input}");
+        Console.WriteLine($"Str2 = {result}");
+    }
+}
