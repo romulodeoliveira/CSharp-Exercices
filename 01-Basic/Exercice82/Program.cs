1d02477c7b39f5d6ea235a6044dfea15b73ff83b
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-21 10:12:20 -0300

    resolução do exercicio 82

--- 01-Basic/Exercice82/Program.cs
+++ 01-Basic/Exercice82/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para remover todos os caracteres que não sejam letras de uma determinada string.
 
 Da Wikipedia,
@@ -11,3 +13,27 @@ Remova todos os caracteres da dita string que não são letras: Python
 String original: 2^sdfds*^*^jlljdslfnoswje34u230sdfds984
 Remova todos os caracteres da referida string que não são letras: sdfdsjlljdslfnoswjeusdfds
 */
+
+namespace Exercice82;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira uma frase: ");
+        string input = Console.ReadLine();
+
+        string result = "";
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (Char.IsLetter(input[i]))
+            {
+                result += input[i];
+            }
+        }
+
+        Console.WriteLine($"Str1: {input}");
+        Console.WriteLine($"Str2: {result}");
+    }
+}
