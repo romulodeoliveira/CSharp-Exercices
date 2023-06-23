db2c4e6b86622c96ddaad00c9d2a489372ce06bc
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-22 23:24:40 -0300

    resolução do exercicio 88

--- 01-Basic/Exercice88/Program.cs
+++ 01-Basic/Exercice88/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para encontrar a soma dos ângulos internos (em graus) de um determinado polígono. Insira o número de linha(s) reta(s).
 
 De Wikipedia,
@@ -7,3 +9,18 @@ Exemplo de saída:
 Insira o número de linhas retas do polígono:
 Soma dos ângulos internos (em graus) do referido polígono: -360
 */
+
+namespace Exercice88;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira o número de linhas retas do polígono: ");
+        int numLines = int.Parse(Console.ReadLine());
+
+        int sumAngles = (numLines - 2) * 180;
+
+        Console.WriteLine($"Soma dos ângulos internos (em graus) do referido polígono: {sumAngles}");
+    }
+}
