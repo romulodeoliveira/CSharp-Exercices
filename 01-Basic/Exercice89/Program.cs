5f220e9263235b8e63052410899093aaa7cc9a9f
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-22 23:41:32 -0300

    resolução do exercicio 89

--- 01-Basic/Exercice89/Program.cs
+++ 01-Basic/Exercice89/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para contar números positivos e negativos em uma determinada matriz de inteiros.
 
 Exemplo de Saída:
@@ -14,3 +16,30 @@ Elementos originais do Array:
 Número de números positivos: 0
 Número de números negativos: 0
 */
+
+namespace Exercice89;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array1 = new int[] { -4, -3, -2, 0, 3, 5, 6, 2, 6 };
+
+        int amountOfPositiveNumbers = 0;
+        int amountOfNegativeNumbers = 0;
+
+        for (int i = 0; i < array1.Length; i++)
+        {
+            if (array1[i] > 0)
+            {
+                amountOfPositiveNumbers += 1;
+            }
+            else
+            {
+                amountOfNegativeNumbers += 1;
+            }
+        }
+
+        Console.WriteLine($"Quantidade de numeros positivos: {amountOfPositiveNumbers}\nQuantidade de numeros negativos: {amountOfNegativeNumbers}");
+    }
+}
