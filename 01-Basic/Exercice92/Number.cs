ff92dadc8ec3b13aae17090581288baaa3bc1a6a
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-24 10:40:28 -0300

    resolução do exercicio 92

---
+++ 01-Basic/Exercice92/Number.cs
@@ -0,0 +1,18 @@
+namespace Exercice92;
+
+public class Number
+{
+    public static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
