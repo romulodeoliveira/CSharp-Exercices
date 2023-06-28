07b9f106493b98addcb0d9066cb7c734c69adacf
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-28 15:40:55 -0300

    resolução do exercicio 96

--- 01-Basic/Exercice96/Program.cs
+++ 01-Basic/Exercice96/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para verificar se todos os caracteres em uma string são iguais. Retorna true se todos os caracteres da string forem iguais, caso contrário, false.
 
 Sample Data:
@@ -7,3 +9,32 @@ Sample Data:
 ("3333") -> True
 ("2342342") -> False
 */
+
+namespace Exercice96;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "aab";
+
+        bool isEqual = true;
+
+        if (isEqual)
+        {
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (i != 0)
+                {
+                    if (str1[i] != str1[i - 1])
+                    {
+                        isEqual = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"É igual: {isEqual}");
+    }
+}
