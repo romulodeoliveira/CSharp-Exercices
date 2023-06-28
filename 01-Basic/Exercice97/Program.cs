e8f80ea7d48ef5c90dafdc446512af387387c904
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-28 15:49:19 -0300

    resolução do exercicio 97

--- 01-Basic/Exercice97/Program.cs
+++ 01-Basic/Exercice97/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para verificar se uma determinada string (ponto flutuante e números negativos incluídos) é numérica ou não. Retorna True se a dita string for numérica caso contrário false.
 
 Sample Data:
@@ -7,3 +9,32 @@ Sample Data:
 ("33/33") -> False
 ("234234d2") -> False
 */
+
+namespace Exercice97;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "123";
+
+        bool isAllNumeric = true;
+
+        if (isAllNumeric)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i != 0)
+                {
+                    if (!char.IsNumber(str[i]))
+                    {
+                        isAllNumeric = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"É tudo numérico: {isAllNumeric}");
+    }
+}
