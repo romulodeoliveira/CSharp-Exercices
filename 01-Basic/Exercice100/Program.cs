b39f0870687b59ebfecb81bdc026be5598635b55
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-01 21:38:13 -0300

    resolução do exercicio 100

--- 01-Basic/Exercice100/Program.cs
+++ 01-Basic/Exercice100/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para verificar a comparação de igualdade (valor e tipo) de dois parâmetros. Retorna verdadeiro se forem iguais, caso contrário, falso.
 
 Sample Data:
@@ -7,3 +9,21 @@ Sample Data:
 (true, "true") -> False
 (10, 10) -> True
 */
+
+namespace Exercice100;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(AreEqual("AAA", "BBB"));
+        Console.WriteLine(AreEqual(true, false));
+        Console.WriteLine(AreEqual(true, "true"));
+        Console.WriteLine(AreEqual(10, 10));
+    }
+
+    static bool AreEqual(object param1, object param2)
+    {
+        return param1.Equals(param2) && param1.GetType() == param2.GetType();
+    }
+}
