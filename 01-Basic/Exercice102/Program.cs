8d34adaedec83f3af5a7b53a620c10ae51d7ff87
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-03 11:38:20 -0300

    resolução do exercicio 102

--- 01-Basic/Exercice102/Program.cs
+++ 01-Basic/Exercice102/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para criar uma matriz identidade.
 
 Dados de amostra:
@@ -7,3 +9,38 @@ Insira um número: 3
 0 1 0
 0 0 1
 */
+
+namespace Exercice102;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira um número: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matriz = new int[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matriz[i, j] = 0;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            matriz[i, i] = 1;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
