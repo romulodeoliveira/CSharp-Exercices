a9fdc9aca2681cd79db2a0cda9b88dff29b010df
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-05 13:59:47 -0300

    resolução do exercicio 10

--- 02-BasicAlgorithm/Exercice10/Program.cs
+++ 02-BasicAlgorithm/Exercice10/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para verificar se um determinado número positivo é um múltiplo de 3 ou um múltiplo de 7.
 
 Exemplo de Entrada :
@@ -13,3 +15,39 @@ Verdadeiro
 Verdadeiro
 Falso
 */
+
+namespace Exercice10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = new int[] { -5, 2, 10, -7, 0, 21, 30, -21, -30 };
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0 && IsMultipleOf3(nums[i]))
+            {
+                Console.WriteLine(true);
+            }
+            else if (nums[i] > 0 && IsMultipleOf7(nums[i]))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+    
+    static bool IsMultipleOf3(int number)
+    {
+        return number % 3 == 0;
+    }
+
+    static bool IsMultipleOf7(int number)
+    {
+        return number % 7 == 0;
+    }
+}
