8037963444a273cbdc48dcb16f9677992002c0de
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-04 23:58:24 -0300

    resolução do exercicio 08

--- 02-BasicAlgorithm/Exercice08/Program.cs
+++ 02-BasicAlgorithm/Exercice08/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para criar uma nova string com 4 cópias dos 2 caracteres frontais de uma determinada string. Se o comprimento da string fornecido for menor que 2, retorne a string original.
 
 Exemplo de entrada :
@@ -12,3 +14,29 @@ JSJSJSJS
 a
 
 */
+
+namespace Exercice08;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] str = new string[] { "C Sharp", "JS", "a" };
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i].Length < 2)
+            {
+                Console.WriteLine(str[i]);
+            }
+            else
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(str[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
