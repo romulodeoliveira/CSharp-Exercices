5b7542df52d18e69f2bd32e016388f12dd418cc9
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-05 13:57:00 -0300

    resolução do exercicio 09

--- 02-BasicAlgorithm/Exercice09/Program.cs
+++ 02-BasicAlgorithm/Exercice09/Program.cs
@@ -1,9 +1,11 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para criar uma nova string com o último caractere adicionado na frente e atrás de uma determinada string de comprimento 1 ou mais.
 
 Entrada de amostra :
-"Vermelho"
-"Verde"
+"Red"
+"Green"
 "1"
 
 Saída esperada :
@@ -11,3 +13,15 @@ dRedd
 nGreenn
 111
 */
+
+namespace Exercice09;
+
+class Pogram
+{
+    static void Main(string[] args)
+    {
+        string str = "Red";
+
+        Console.WriteLine($"{str[str.Length - 1]}{str}{str[str.Length - 1]}");
+    }
+}
