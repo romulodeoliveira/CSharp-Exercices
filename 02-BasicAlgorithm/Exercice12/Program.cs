1fdee24567277613def29e86dc2774c53cdfeae0
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-06 14:37:54 -0300

    resolução do exercicio 12

--- 02-BasicAlgorithm/Exercice12/Program.cs
+++ 02-BasicAlgorithm/Exercice12/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para verificar se uma determinada string começa com 'C#' ou não.
 
 Exemplo de entrada :
@@ -11,3 +13,25 @@ Verdadeiro
 Verdadeiro
 Falso
 */
+
+namespace Exercice12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] str = new string[] { "Python", "JS", "CSharp", "C#"};
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i][0] == 'C' && str[i][1] == '#')
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
