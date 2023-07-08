d93e321254113d8615a6caa6af7ab16c34a4ecde
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-08 13:51:07 -0300

    resolução do exercicio 17

--- 02-BasicAlgorithm/Exercice17/Program.cs
+++ 02-BasicAlgorithm/Exercice17/Program.cs
@@ -1,13 +1,39 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para verificar se uma string 'yt' aparece no índice 1 em uma determinada string. Se aparecer, retorne uma string sem 'yt' caso contrário, retorne a string original.
 
-Exemplo de entrada :
+Sample Input:
 "Python"
 "ytade"
 "jsues"
 
-Saída esperada :
-telefone
-veio à tona
+Expected Output:
+Phon
+ytade
 jsues
 */
+
+namespace Exercice17;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RemoveYT("Python"));
+        Console.WriteLine(RemoveYT("ytade"));
+        Console.WriteLine(RemoveYT("jsues"));
+    }
+
+    static string RemoveYT(string input)
+    {
+        if (input.Length > 1 && input.Substring(1, 2) == "yt")
+        {
+            return input.Remove(1, 2);
+        }
+        else
+        {
+            return input;
+        }
+    }
+}
