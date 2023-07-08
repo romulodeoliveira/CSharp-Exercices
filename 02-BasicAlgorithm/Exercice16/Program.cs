c97d99e5a6eda18ba367d87e9c868edfd5c29dc8
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-08 13:46:23 -0300

    resolução do exercicio 16

--- 02-BasicAlgorithm/Exercice16/Program.cs
+++ 02-BasicAlgorithm/Exercice16/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para verificar se dois valores inteiros fornecidos estão no intervalo de 20 a 50 inclusive. Retorna verdadeiro se 1 ou outro estiver no referido intervalo, caso contrário, falso.
 
 Entrada de amostra :
@@ -13,3 +15,26 @@ Verdadeiro
 Verdadeiro
 Falso
 */
+
+namespace Exercice16;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] values1 = new int[] { 20, 84 };
+        int[] values2 = new int[] { 14, 50 };
+        int[] values3 = new int[] { 11, 45 };
+        int[] values4 = new int[] { 25, 40 };
+
+        bool result1 = values1[0] >= 20 && values1[0] <= 50 && values1[1] < 20 || values1[1] > 50 || values1[1] >= 20 && values1[1] <= 50 && values1[0] < 20 || values1[0] > 50;
+        bool result2 = values2[0] >= 20 && values2[0] <= 50 && values2[1] < 20 || values2[1] > 50 || values2[1] >= 20 && values2[1] <= 50 && values2[0] < 20 || values2[0] > 50;
+        bool result3 = values3[0] >= 20 && values3[0] <= 50 && values3[1] < 20 || values3[1] > 50 || values3[1] >= 20 && values3[1] <= 50 && values3[0] < 20 || values3[0] > 50;
+        bool result4 = values4[0] >= 20 && values4[0] <= 50 && values4[1] < 20 || values4[1] > 50 || values4[1] >= 20 && values4[1] <= 50 && values4[0] < 20 || values4[0] > 50;
+
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+        Console.WriteLine(result3);   
+        Console.WriteLine(result4);   
+    }
+}
