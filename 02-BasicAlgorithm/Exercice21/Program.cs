32fb5555fa634682c7fc8c154a62d15ce71b1af3
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-09 13:48:55 -0300

    resolução do exercicio 21

--- 02-BasicAlgorithm/Exercice21/Program.cs
+++ 02-BasicAlgorithm/Exercice21/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para encontrar o maior valor de dois valores inteiros positivos que estejam no intervalo de 20 a 30, inclusive, ou retorne 0 se nenhum estiver nesse intervalo.
 
 Entrada de amostra :
@@ -13,3 +15,39 @@ Saída esperada :
 25
 28
 */
+
+namespace Exercice21;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(FindLargestInRange(78, 95));
+        Console.WriteLine(FindLargestInRange(20, 30));
+        Console.WriteLine(FindLargestInRange(21, 25));
+        Console.WriteLine(FindLargestInRange(28, 28));
+    }
+
+    static int FindLargestInRange(int number1, int number2)
+    {
+        bool inRange1 = number1 >= 20 && number1 <= 30;
+        bool inRange2 = number2 >= 20 && number2 <= 30;
+
+        if (!inRange1 && !inRange2)
+        {
+            return 0;
+        }
+        else if (inRange1 && inRange2)
+        {
+            return Math.Max(number1, number2);
+        }
+        else if (inRange1)
+        {
+            return number1;
+        }
+        else
+        {
+            return number2;
+        }
+    }
+}
