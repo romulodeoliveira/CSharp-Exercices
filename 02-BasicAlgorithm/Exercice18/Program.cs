60f0975aded45ab566580b0a6e1be23853641ce1
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-09 13:13:07 -0300

    resolução do exercicio 18

--- 02-BasicAlgorithm/Exercice18/Program.cs
+++ 02-BasicAlgorithm/Exercice18/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para verificar o maior número entre três inteiros dados.
 
 Entrada de amostra :
@@ -13,3 +15,33 @@ Saída esperada :
 1
 2
 */
+
+namespace Exercice18;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetLargestNumber(1, 2, 3));
+        Console.WriteLine(GetLargestNumber(1, 3, 2));
+        Console.WriteLine(GetLargestNumber(1, 1, 1));
+        Console.WriteLine(GetLargestNumber(1, 2, 2));
+    }
+
+    static int GetLargestNumber(int number1, int number2, int number3)
+    {
+        int largest = number1;
+
+        if (number2 > largest)
+        {
+            largest = number2;
+        }
+
+        if (number3 > largest)
+        {
+            largest = number3;
+        }
+
+        return largest;
+    }
+}
