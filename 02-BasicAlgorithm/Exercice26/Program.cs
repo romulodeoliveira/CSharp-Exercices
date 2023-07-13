f8f122adcb31582ab316d108a613f2078df8055b
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-12 23:13:34 -0300

    resolução do exercicio 26

--- 02-BasicAlgorithm/Exercice26/Program.cs
+++ 02-BasicAlgorithm/Exercice26/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para criar uma nova string que é n (número inteiro não negativo) cópias dos primeiros 3 caracteres de uma determinada string. Se o comprimento da string fornecida for menor que 3, retorne n cópias da string.
 
 Exemplo de entrada :
@@ -7,7 +9,26 @@ Exemplo de entrada :
 "JS", 3
 
 Saída esperada :
-PtPt
+PytPyt
 PytPytPyt
 JSJSJS
+
 */
+
+namespace Exercice26;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CopyFirstThreeChars("Python", 2));
+        Console.WriteLine(CopyFirstThreeChars("Python", 3));
+        Console.WriteLine(CopyFirstThreeChars("JS", 3));
+    }
+
+    static string CopyFirstThreeChars(string str, int n)
+    {
+        string firstThreeChars = str.Length >= 3 ? str.Substring(0, 3) : str;
+        return String.Concat(Enumerable.Repeat(firstThreeChars, n));
+    }
+}
