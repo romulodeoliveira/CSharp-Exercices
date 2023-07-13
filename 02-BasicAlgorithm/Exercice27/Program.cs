bdc8e55daa41e966b6f958c95b253ed8696d1dca
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-12 23:40:59 -0300

    resolução do exercicio 27

--- 02-BasicAlgorithm/Exercice27/Program.cs
+++ 02-BasicAlgorithm/Exercice27/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para contar a string "aa" em uma determinada string e assumir que "aaa" contém dois "aa".
 
 Exemplo de entrada:
@@ -11,3 +13,28 @@ Saída esperada:
 2
 3
 */
+
+namespace Exercice27;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CountSubstringOccurrences("bbaaccaag", "aa"));
+        Console.WriteLine(CountSubstringOccurrences("jjkiaaasew", "aa"));
+        Console.WriteLine(CountSubstringOccurrences("JSaaakoiaa", "aa"));
+    }
+
+    static int CountSubstringOccurrences(string input, string substring)
+    {
+        int count = 0;
+        int index = 0;
+        while ((index = input.IndexOf(substring, index)) != -1)
+        {
+            count++;
+            index += substring.Length - 1;
+        }
+
+        return count;
+    }
+}
