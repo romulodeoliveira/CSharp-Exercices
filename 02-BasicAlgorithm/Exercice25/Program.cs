f7abf2eea72e55c727fc5179f7dc39a0a08e6316
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-12 22:55:13 -0300

    resolução do exercicio 25

--- 02-BasicAlgorithm/Exercice25/Program.cs
+++ 02-BasicAlgorithm/Exercice25/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para criar uma nova string que é n (número inteiro não negativo) cópias de uma determinada string.
 
 Entrada de amostra :
@@ -11,3 +13,20 @@ JSJS
 JSJSJS
 js
 */
+
+namespace Exercice25;
+    
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CopyString("JS", 2));
+        Console.WriteLine(CopyString("JS", 3));
+        Console.WriteLine(CopyString("JS", 1));
+    }
+
+    static string CopyString(string str, int n)
+    {
+        return String.Concat(Enumerable.Repeat(str, n));
+    }
+}
