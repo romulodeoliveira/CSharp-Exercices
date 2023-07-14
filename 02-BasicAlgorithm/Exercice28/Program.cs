4da0e6141a5b9948c7e71ee98ec9df7c829b2374
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-13 21:57:33 -0300

    resolução do exercicio 28

--- 02-BasicAlgorithm/Exercice28/Program.cs
+++ 02-BasicAlgorithm/Exercice28/Program.cs
@@ -11,3 +11,30 @@ Verdadeiro
 Falso
 Verdadeiro
 */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = { "caabb", "babaaba", "aaaaa" };
+
+        Console.WriteLine($"{inputs[0]}: {CheckDoubleA(inputs[0])}");
+        Console.WriteLine($"{inputs[1]}: {CheckDoubleA(inputs[1])}");
+        Console.WriteLine($"{inputs[2]}: {CheckDoubleA(inputs[2])}");
+    }
+
+    static bool CheckDoubleA(string input)
+    {
+        int firstAIndex = input.IndexOf('a');
+
+        if (firstAIndex != -1 && firstAIndex + 1 < input.Length && input[firstAIndex + 1] == 'a')
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
