1d2ee5e07875316703b56dd30e0252bd4883a776
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-15 15:21:19 -0300

    resolução do exercicio 30

--- 02-BasicAlgorithm/Exercice30/Program.cs
+++ 02-BasicAlgorithm/Exercice30/Program.cs
@@ -1,13 +1,27 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para criar uma string como "aababcabcd" a partir de uma determinada string "abcd".
 
-Exemplo de entrada:
+Sample Input:
 "abcd"
 "abc"
 "a"
 
-Saída esperada:
-pai
-pai
+Expected Output:
+aababcabcd
+aababc
 a
 */
+
+namespace Exercice30;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "abcd";
+
+        Console.WriteLine($"{str[0]}{str[0]}{str[1]}{str[0]}{str[1]}{str[2]}{str[0]}{str[1]}{str[2]}{str[3]}");
+    }
+}
