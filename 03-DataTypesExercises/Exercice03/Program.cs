c172348a90f67b17049027874a09842fe2f200bc
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-16 23:36:09 -0300

    resolução do exercicio 03

--- 03-DataTypesExercises/Exercice03/Program.cs
+++ 03-DataTypesExercises/Exercice03/Program.cs
@@ -1,1 +1,41 @@
-﻿// Escreva um programa C# Sharp que receba o ID do usuário e a senha como entrada (tipo string). Após 3 tentativas erradas, o usuário será rejeitado.
+﻿using System;
+
+// Escreva um programa C# Sharp que receba o ID do usuário e a senha como entrada (tipo string). Após 3 tentativas erradas, o usuário será rejeitado.
+
+namespace Exercice03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string idCorreto = "usuario";
+        string senhaCorreta = "senha";
+        int maxTentativas = 3;
+        int tentativas = 0;
+
+        while (tentativas < maxTentativas)
+        {
+            Console.Write("Digite o ID do usuário: ");
+            string id = Console.ReadLine();
+
+            Console.Write("Digite a senha: ");
+            string senha = Console.ReadLine();
+
+            if (id == idCorreto && senha == senhaCorreta)
+            {
+                Console.WriteLine("Login bem-sucedido!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("ID do usuário ou senha incorretos. Tente novamente.");
+                tentativas++;
+            }
+        }
+
+        if (tentativas == maxTentativas)
+        {
+            Console.WriteLine("Número máximo de tentativas excedido. Acesso negado.");
+        }
+    }
+}
