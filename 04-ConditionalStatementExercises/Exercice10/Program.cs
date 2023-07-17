ef7f307d097a607a55e9a083e2c83166dc747bfc
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-16 23:58:34 -0300

    resolução do exercicio 10

--- 04-ConditionalStatementExercises/Exercice10/Program.cs
+++ 04-ConditionalStatementExercises/Exercice10/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para encontrar a elegibilidade de admissão para um curso profissional com base nos seguintes critérios:
 
 Notas em matemática >=65
@@ -16,3 +18,35 @@ Informe as notas obtidas em Matemática :72
 Resultado Esperado :
 O candidato é elegível para admissão.
 */
+
+namespace Exercice10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe as notas obtidas em Física: ");
+        int notaFisica = int.Parse(Console.ReadLine());
+
+        Console.Write("Informe as notas obtidas em Química: ");
+        int notaQuimica = int.Parse(Console.ReadLine());
+
+        Console.Write("Informe as notas obtidas em Matemática: ");
+        int notaMatematica = int.Parse(Console.ReadLine());
+
+        int totalNotas = notaFisica + notaQuimica + notaMatematica;
+
+        if (notaMatematica >= 65 && notaFisica >= 55 && notaQuimica >= 50 && totalNotas >= 180)
+        {
+            Console.WriteLine("O candidato é elegível para admissão.");
+        }
+        else if (totalNotas >= 140)
+        {
+            Console.WriteLine("O candidato é elegível para admissão.");
+        }
+        else
+        {
+            Console.WriteLine("O candidato não é elegível para admissão.");
+        }
+    }
+}
