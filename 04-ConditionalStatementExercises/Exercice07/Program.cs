6260f510cf53eb6d2075cd5983a401ed63308903
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-16 23:45:12 -0300

    resolução do exercicio 07

--- 04-ConditionalStatementExercises/Exercice07/Program.cs
+++ 04-ConditionalStatementExercises/Exercice07/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para aceitar a altura de uma pessoa em centímetros e categorizá-la de acordo com sua altura.
 
 Dados de teste: 135
@@ -6,3 +8,39 @@ Dados de teste: 135
 Saída esperada :
 A pessoa é Anão.
 */
+
+namespace Exercice07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a altura em centímetros: ");
+        int altura = int.Parse(Console.ReadLine());
+
+        string categoria;
+
+        if (altura < 150)
+        {
+            categoria = "Anão";
+        }
+        else if (altura < 165)
+        {
+            categoria = "Baixo";
+        }
+        else if (altura < 180)
+        {
+            categoria = "Médio";
+        }
+        else if (altura < 195)
+        {
+            categoria = "Alto";
+        }
+        else
+        {
+            categoria = "Muito Alto";
+        }
+
+        Console.WriteLine($"A pessoa é {categoria}.");
+    }
+}
