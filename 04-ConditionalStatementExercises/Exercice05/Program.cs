5121e3f60f985ca2cd0ee38850e799fd49e87f78
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-16 23:38:55 -0300

    resolução do exercicio 05

--- 04-ConditionalStatementExercises/Exercice05/Program.cs
+++ 04-ConditionalStatementExercises/Exercice05/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para ler a idade de um candidato e determinar se ele é elegível para votar.
 
 Dados de teste: 21
@@ -6,3 +8,23 @@ Dados de teste: 21
 Saída esperada :
 Parabéns! Você é elegível para votar.
 */
+
+namespace Exercice05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a idade do candidato: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("Parabéns! Você é elegível para votar.");
+        }
+        else
+        {
+            Console.WriteLine("Desculpe, você não é elegível para votar.");
+        }
+    }
+}
