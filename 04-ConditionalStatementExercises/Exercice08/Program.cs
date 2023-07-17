952969c4b0332b4be76c23b6370c63d7b342b060
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-16 23:54:09 -0300

    resolução do exercicio 08

--- 04-ConditionalStatementExercises/Exercice08/Program.cs
+++ 04-ConditionalStatementExercises/Exercice08/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para encontrar o maior de três números.
 
 Dados de teste:
@@ -9,3 +11,39 @@ Insira o 3º número: 10
 Saída esperada :
 O 2º número é o maior entre três
 */
+
+namespace Exercice08;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira o 1º número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Insira o 2º número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Insira o 3º número: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        int maior = 0;
+        
+        if (num1 > num2 && num1 > num3)
+        {
+            maior = 1;
+        }
+
+        else if (num2 > num3)
+        {
+            maior = 2;
+        }
+
+        else
+        {
+            maior = 3;
+        }
+
+        Console.WriteLine($"O {maior}º número é o maior entre os três.");
+    }
+}
