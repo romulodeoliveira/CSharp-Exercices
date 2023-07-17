e747de25a6aa1af2610dbdd1b85d077ee2c6c84a
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-16 23:56:38 -0300

    resolução do exercicio 09

--- 04-ConditionalStatementExercises/Exercice09/Program.cs
+++ 04-ConditionalStatementExercises/Exercice09/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para aceitar um ponto de coordenada em um sistema de coordenadas XY e determinar em qual quadrante o ponto de coordenada está.
 
 Dados de teste:
@@ -8,3 +10,46 @@ Insira o valor para a coordenada Y:9
 Saída esperada :
 O ponto de coordenada (7,9) está no primeiro quadrante.
 */
+
+namespace Exercice09;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira o valor para a coordenada X: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.Write("Insira o valor para a coordenada Y: ");
+        int y = int.Parse(Console.ReadLine());
+
+        if (x > 0 && y > 0)
+        {
+            Console.WriteLine($"O ponto de coordenada ({x},{y}) está no primeiro quadrante.");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine($"O ponto de coordenada ({x},{y}) está no segundo quadrante.");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine($"O ponto de coordenada ({x},{y}) está no terceiro quadrante.");
+        }
+        else if (x > 0 && y < 0)
+        {
+            Console.WriteLine($"O ponto de coordenada ({x},{y}) está no quarto quadrante.");
+        }
+        else if (x == 0 && y != 0)
+        {
+            Console.WriteLine($"O ponto de coordenada ({x},{y}) está sobre o eixo Y.");
+        }
+        else if (x != 0 && y == 0)
+        {
+            Console.WriteLine($"O ponto de coordenada ({x},{y}) está sobre o eixo X.");
+        }
+        else
+        {
+            Console.WriteLine($"O ponto de coordenada ({x},{y}) está na origem.");
+        }
+    }
+}
