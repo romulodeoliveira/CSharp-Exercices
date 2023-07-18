a2d125018954bb397c19541a21c345178f60625d
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-18 20:12:57 -0300

    resolução do exercicio 11

--- 04-ConditionalStatementExercises/Exercice11/Program.cs
+++ 04-ConditionalStatementExercises/Exercice11/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para calcular a raiz da equação quadrática.
 
 Dados de teste:
@@ -10,3 +12,36 @@ Saída esperada :
 As raízes são imaginárias;
 Nenhuma solução.
 */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe o valor de a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o valor de b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o valor de c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0)
+        {
+            double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+            Console.WriteLine($"As raízes são: {root1} e {root2}");
+        }
+        else if (discriminant == 0)
+        {
+            double root = -b / (2 * a);
+            Console.WriteLine($"A única raiz é: {root}");
+        }
+        else
+        {
+            Console.WriteLine("As raízes são imaginárias; nenhuma solução.");
+        }
+    }
+}
