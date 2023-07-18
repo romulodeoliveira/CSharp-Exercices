c885b7571c2e43d482d00ca246bb4b6c1ad6cfba
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-17 22:47:13 -0300

    resolução do exercicio 29

--- 02-BasicAlgorithm/Exercice29/Program.cs
+++ 02-BasicAlgorithm/Exercice29/Program.cs
@@ -11,3 +11,29 @@ tomada de força
 PP
 J
 */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = { "Python", "PHP", "JS" };
+
+        foreach (string input in inputs)
+        {
+            string result = CreateNewString(input);
+            Console.WriteLine(result);
+        }
+    }
+
+    static string CreateNewString(string input)
+    {
+        string newString = "";
+        for (int i = 0; i < input.Length; i += 2)
+        {
+            newString += input[i];
+        }
+        return newString;
+    }
+}
