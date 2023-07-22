830cd534ae95050bfa97c4b07ba5f5aa2255b270
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-22 19:27:19 -0300

    resolução do exercicio 16

--- 04-ConditionalStatementExercises/Exercice16/Program.cs
+++ 04-ConditionalStatementExercises/Exercice16/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para verificar se um alfabeto é uma vogal ou consoante.
 
 Dados de teste:
@@ -7,3 +9,37 @@ k
 Saída esperada :
 O alfabeto é uma consoante.
 */
+
+namespace Exercice16;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite uma letra do alfabeto: ");
+        char letter = Char.ToLower(Console.ReadKey().KeyChar);
+
+        if (Char.IsLetter(letter))
+        {
+            if (IsVowel(letter))
+            {
+                Console.WriteLine("\nA letra é uma vogal.");
+            }
+            else
+            {
+                Console.WriteLine("\nA letra é uma consoante.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("\nEntrada inválida. Por favor, insira apenas uma letra do alfabeto.");
+        }
+    }
+
+    static bool IsVowel(char letter)
+    {
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+        return Array.Exists(vowels, vowel => vowel == letter);
+    }
+}
+
