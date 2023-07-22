894c00c6749bbb448c5c112bdbb3b1847b7d3e8b
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-22 19:11:37 -0300

    resolução do exercicio 15

--- 04-ConditionalStatementExercises/Exercice15/Program.cs
+++ 04-ConditionalStatementExercises/Exercice15/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para verificar se um triângulo pode ser formado pelo valor dado para os ângulos.
 
 Dados de teste:
@@ -7,3 +9,38 @@ Dados de teste:
 Saída esperada :
 O triângulo não é válido.
 */
+
+namespace Exercice15;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite os três valores dos ângulos do triângulo (separados por espaço):");
+
+        string input = Console.ReadLine();
+        string[] angles = input.Split(' ');
+
+        if (angles.Length != 3)
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira três valores separados por espaço.");
+            return;
+        }
+
+        double angle1, angle2, angle3;
+        if (!double.TryParse(angles[0], out angle1) || !double.TryParse(angles[1], out angle2) || !double.TryParse(angles[2], out angle3))
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira apenas valores numéricos válidos.");
+            return;
+        }
+
+        if (angle1 + angle2 + angle3 == 180)
+        {
+            Console.WriteLine("O triângulo é válido.");
+        }
+        else
+        {
+            Console.WriteLine("O triângulo não é válido.");
+        }
+    }
+}
