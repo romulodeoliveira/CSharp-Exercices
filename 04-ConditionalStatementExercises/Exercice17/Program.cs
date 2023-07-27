fd0fddfef3975bc98363b8948ab5c7893b4bc6bc
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-26 23:50:35 -0300

    resolução do exercicio 17

--- 04-ConditionalStatementExercises/Exercice17/Program.cs
+++ 04-ConditionalStatementExercises/Exercice17/Program.cs
@@ -7,3 +7,41 @@ Dados de teste:
 Saída esperada :
 Você pode reservar o valor do lucro: 200
 */
+
+using System;
+
+namespace Exercice17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de compra: ");
+            if (double.TryParse(Console.ReadLine(), out double buyingPrice))
+            {
+                Console.Write("Digite o valor de venda: ");
+                if (double.TryParse(Console.ReadLine(), out double sellingPrice))
+                {
+                    double profit = sellingPrice - buyingPrice;
+
+                    if (profit >= 0)
+                    {
+                        Console.WriteLine($"Você pode reservar o valor do lucro: {profit}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Você teve uma perda de: {Math.Abs(profit)}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Valor de venda inválido. Por favor, insira um valor numérico válido.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor de compra inválido. Por favor, insira um valor numérico válido.");
+            }
+        }
+    }
+}
