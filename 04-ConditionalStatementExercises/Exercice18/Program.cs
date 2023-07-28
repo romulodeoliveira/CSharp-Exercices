8cc7c4a234c0de47ab46b878bd80310e24c1a934
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-27 21:59:11 -0300

    resolução do exercicio 18]

--- 04-ConditionalStatementExercises/Exercice18/Program.cs
+++ 04-ConditionalStatementExercises/Exercice18/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa em C# Sharp para calcular e imprimir a conta de luz de um determinado cliente. O ID do cliente, nome e unidade consumida pelo usuário devem ser retirados do teclado e exibir o valor total a pagar ao cliente. A cobrança é a seguinte:
 
 Unidade	Carga/unidade
@@ -21,3 +23,55 @@ Valor cobrados @Rs. 2,00 por unidade: 1600,00
 Valor da sobretaxa: 240,00
 Valor líquido pago pelo cliente: 1840,00
 */
+
+namespace Exercice18;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o ID do cliente: ");
+        int customerId = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o nome do cliente: ");
+        string customerName = Console.ReadLine();
+
+        Console.Write("Digite a quantidade de unidades consumidas pelo cliente: ");
+        int unitsConsumed = int.Parse(Console.ReadLine());
+
+        double chargePerUnit;
+        if (unitsConsumed <= 199)
+        {
+            chargePerUnit = 1.20;
+        }
+        else if (unitsConsumed >= 200 && unitsConsumed < 400)
+        {
+            chargePerUnit = 1.50;
+        }
+        else if (unitsConsumed >= 400 && unitsConsumed < 600)
+        {
+            chargePerUnit = 1.80;
+        }
+        else
+        {
+            chargePerUnit = 2.00;
+        }
+
+        double totalAmount = unitsConsumed * chargePerUnit;
+
+        if (totalAmount > 400)
+        {
+            double surcharge = totalAmount * 0.15;
+            totalAmount += surcharge;
+        }
+        else if (totalAmount < 100)
+        {
+            totalAmount = 100;
+        }
+
+        Console.WriteLine($"\nID do cliente: {customerId}");
+        Console.WriteLine($"Nome do cliente: {customerName}");
+        Console.WriteLine($"Unidades consumidas: {unitsConsumed}");
+        Console.WriteLine($"Valor cobrado @R$ {chargePerUnit:0.00} por unidade: R$ {totalAmount:0.00}");
+    }
+}
