011d5dfeb66b78ea0712cee5a2ecee79b1b061e2
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-28 22:11:17 -0300

    resolução do exercicio 19

--- 04-ConditionalStatementExercises/Exercice19/Program.cs
+++ 04-ConditionalStatementExercises/Exercice19/Program.cs
@@ -13,3 +13,42 @@ Insira a nota:a
 Saída esperada :
 Você escolheu: Média
 */
+
+using System;
+
+namespace GradeDescription;
+    
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a nota: ");
+        char grade = char.ToLower(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        string description;
+        switch (grade)
+        {
+            case 'e':
+                description = "Excelente";
+                break;
+            case 'v':
+                description = "Muito bom";
+                break;
+            case 'g':
+                description = "Bom";
+                break;
+            case 'a':
+                description = "Média";
+                break;
+            case 'f':
+                description = "Falhar";
+                break;
+            default:
+                description = "Nota inválida";
+                break;
+        }
+
+        Console.WriteLine($"Você escolheu: {description}");
+    }
+}
