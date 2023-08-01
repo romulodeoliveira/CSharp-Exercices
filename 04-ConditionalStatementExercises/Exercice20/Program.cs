37a8bb366200425897cea73525423c2065d08b07
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-31 22:13:12 -0300

    resolução do exercicio 20

--- 04-ConditionalStatementExercises/Exercice20/Program.cs
+++ 04-ConditionalStatementExercises/Exercice20/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa em C# Sharp para ler qualquer número de dia inteiro e exibir o nome do dia na palavra.
 
 Dados de teste:
@@ -7,3 +9,51 @@ Dados de teste:
 Saída esperada :
 quinta-feira
 */
+
+namespace Exercice20;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número de 1 a 7: ");
+        int number = int.Parse(Console.ReadLine());
+
+        string result = "";
+
+        if (number == 1)
+        {
+            result = "Domingo";
+        }
+        else if (number == 2)
+        {
+            result = "Segunda-feira";
+        }
+        else if (number == 3)
+        {
+            result = "Terça-feira";
+        }
+        else if (number == 4)
+        {
+            result = "Quarta-feira";
+        }
+        else if (number == 5)
+        {
+            result = "Quinta-feira";
+        }
+        else if (number == 6)
+        {
+            result = "Sexta-feira";
+        }
+        else if (number == 7)
+        {
+            result = "Sábado";
+        }
+        else
+        {
+            Console.WriteLine("Número inválido");
+        }
+
+        Console.WriteLine(result);
+    }
+}
