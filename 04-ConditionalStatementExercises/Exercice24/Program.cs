928f0269e70ef2165098bef81b181fb136151fb3
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-03 21:13:09 -0300

    resolução do exercicio 24

--- 04-ConditionalStatementExercises/Exercice24/Program.cs
+++ 04-ConditionalStatementExercises/Exercice24/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa em C# Sharp que seja um programa orientado por menus para calcular a área de várias formas geométricas.
 
 Dados de teste:
@@ -8,3 +10,41 @@ Insira o raio do círculo: 5
 Saída esperada :
 A área é: 78,500000
 */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Calcular área do círculo");
+            Console.WriteLine("2. Sair");
+            Console.Write("Insira sua escolha: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    CalcularAreaCirculo();
+                    break;
+                case 2:
+                    Console.WriteLine("Encerrando o programa.");
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+
+    static void CalcularAreaCirculo()
+    {
+        Console.Write("Insira o raio do círculo: ");
+        double radius = double.Parse(Console.ReadLine());
+
+        double area = Math.PI * radius * radius;
+
+        Console.WriteLine($"A área é: {area.ToString("F6")}");
+    }
+}
