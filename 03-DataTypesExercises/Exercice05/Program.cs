a6f8154a87d439b5e6afa28ec7a63216654100cb
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-05 13:32:43 -0300

    resolução do exercicio 05

--- 03-DataTypesExercises/Exercice05/Program.cs
+++ 03-DataTypesExercises/Exercice05/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp que tome o raio de um círculo como entrada e calcule o perímetro e a área do círculo.
 
 Dados de teste
@@ -7,3 +9,18 @@ Insira o raio do círculo:
 Saída esperada :
 Perímetro do círculo: 75,36
 */
+
+namespace Exercice05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira o raio do círculo: ");
+        double radius = double.Parse(Console.ReadLine());
+
+        double perimeter = 2 * Math.PI * radius;
+
+        Console.WriteLine($"Perímetro do círculo: {perimeter.ToString("F2")}");
+    }
+}
