e2e93188955ecc4fa47fe34f494d2721615869dd
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-06 22:46:49 -0300

    resolução do exercicio 09

--- 03-DataTypesExercises/Exercice09/Program.cs
+++ 03-DataTypesExercises/Exercice09/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp que receba um caractere como entrada e verifique se a entrada (minúscula) é uma vogal, um dígito ou qualquer outro símbolo.
 
 Dados de teste:
@@ -6,3 +8,27 @@ Insira um símbolo: a
 Saída esperada:
 É uma vogal minúscula.
 */
+
+namespace Exercice09;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira um símbolo: ");
+        char symbol = char.Parse(Console.ReadLine());
+
+        if (Char.IsDigit(symbol))
+        {
+            Console.WriteLine("É um dígito.");
+        }
+        else if (symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u')
+        {
+            Console.WriteLine("É uma vogal minúscula.");
+        }
+        else
+        {
+            Console.WriteLine("É qualquer outro símbolo.");
+        }
+    }
+}
