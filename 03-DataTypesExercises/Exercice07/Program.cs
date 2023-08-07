21e515497ee95ee5a58a0818eb2167b4c27f48ca
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-06 22:27:03 -0300

    resolução do exercicio 07

--- 03-DataTypesExercises/Exercice07/Program.cs
+++ 03-DataTypesExercises/Exercice07/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa em C# Sharp que tome distância e tempo como entrada e exiba a velocidade em quilômetros por hora e milhas por hora.
 
 Dados de teste:
@@ -11,3 +13,32 @@ Sua velocidade em metros/s é 8,686588
 Sua velocidade em km /h é 31,27172
 Sua velocidade em milhas/h é 19,4355
 */
+
+namespace Exercice07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Distância de entrada (metros): ");
+        double distanceMeters = double.Parse(Console.ReadLine());
+
+        Console.Write("Tempo de entrada (horas): ");
+        double timeHours = double.Parse(Console.ReadLine());
+
+        Console.Write("Tempo de entrada (minutos): ");
+        double timeMinutes = double.Parse(Console.ReadLine());
+
+        Console.Write("Tempo de entrada (segundos): ");
+        double timeSeconds = double.Parse(Console.ReadLine());
+
+        double totalTimeHours = timeHours + timeMinutes / 60 + timeSeconds / 3600;
+        double speedMetersPerSecond = distanceMeters / totalTimeHours;
+        double speedKmPerHour = speedMetersPerSecond * 3.6;
+        double speedMilesPerHour = speedKmPerHour / 1.60934;
+
+        Console.WriteLine($"Sua velocidade em metros/s é {speedMetersPerSecond:F6}");
+        Console.WriteLine($"Sua velocidade em km/h é {speedKmPerHour:F6}");
+        Console.WriteLine($"Sua velocidade em milhas/h é {speedMilesPerHour:F6}");
+    }
+}
