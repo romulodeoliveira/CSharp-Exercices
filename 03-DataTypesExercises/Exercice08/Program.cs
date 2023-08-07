c738b6d4e11133ca24e79f3f4e4e3770e1d7a2b9
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-06 22:34:28 -0300

    resolução do exercicio 08

--- 03-DataTypesExercises/Exercice08/Program.cs
+++ 03-DataTypesExercises/Exercice08/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp que tome o raio de uma esfera como entrada e calcule e exiba a superfície e o volume da esfera.
 
 Dados de teste:
@@ -7,3 +9,20 @@ Saída esperada:
 50,26548
 33,51032
 */
+
+namespace Exercice08;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Raio: ");
+        double radius = double.Parse(Console.ReadLine());
+
+        double surfaceArea = 4 * Math.PI * radius * radius;
+        double volume = 4.0 / 3.0 * Math.PI * Math.Pow(radius, 3);
+
+        Console.WriteLine(surfaceArea.ToString("F5"));
+        Console.WriteLine(volume.ToString("F5"));
+    }
+}
