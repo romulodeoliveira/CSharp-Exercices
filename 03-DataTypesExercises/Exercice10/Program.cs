f9dc1ccb43e2c4ab80af2144e865dc0507984966
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-07 21:12:02 -0300

    resolução do exercicio 10

--- 03-DataTypesExercises/Exercice10/Program.cs
+++ 03-DataTypesExercises/Exercice10/Program.cs
@@ -1,3 +1,29 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp que receba dois números como entrada e retorne verdadeiro ou falso quando ambos os números forem pares ou ímpares.
 */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        bool areEven = (num1 % 2 == 0) && (num2 % 2 == 0);
+        bool areOdd = (num1 % 2 != 0) && (num2 % 2 != 0);
+
+        if (areEven || areOdd)
+        {
+            Console.WriteLine("Verdadeiro");
+        }
+        else
+        {
+            Console.WriteLine("Falso");
+        }
+    }
+}
