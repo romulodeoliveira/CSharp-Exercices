8a5bb8db686e5ab4888fd400c26beb0e9823854e
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-13 01:27:46 -0300

    resolução do exercicio 05

--- 05-ForLoopExercises/Exercice05/Program.cs
+++ 05-ForLoopExercises/Exercice05/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para exibir o cubo de um número inteiro até determinado número.
 Dados de teste:
 Insira o número de termos: 5
@@ -9,3 +11,20 @@ O número é: 3 e o cubo de 3 é: 27
 O número é : 4 e o cubo do 4 é :64
 O número é : 5 e o cubo do 5 é :125
 */
+
+namespace Exercice05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira o número de termos: ");
+        int numTerms = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= numTerms; i++)
+        {
+            int cube = i * i * i;
+            Console.WriteLine($"O número é: {i} e o cubo de {i} é: {cube}");
+        }
+    }
+}
