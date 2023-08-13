ca6f2d3904317dd427de0ba8bdcbd8b1a98bfcab
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-12 22:52:19 -0300

    resolução do exercicio 04

--- 05-ForLoopExercises/Exercice04/Program.cs
+++ 05-ForLoopExercises/Exercice04/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para ler 10 números e encontrar sua média e soma.
 Dados de teste:
 Insira os 10 números:
@@ -9,3 +11,28 @@ Saída esperada :
 A soma de 10 não é: 51
 A média é: 5,100000
 */
+
+namespace Exercice04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int quantityNumbers = 10;
+        int sum = 0;
+
+        Console.WriteLine($"Insira os {quantityNumbers} números:");
+
+        for (int i = 1; i <= quantityNumbers; i++)
+        {
+            Console.Write($"Número-{i}: ");
+            int number = int.Parse(Console.ReadLine());
+            sum += number;
+        }
+
+        double average = (double)sum / quantityNumbers;
+
+        Console.WriteLine($"A soma dos {quantityNumbers} números é: {sum}");
+        Console.WriteLine($"A média é: {average.ToString("F6")}");
+}
+}
