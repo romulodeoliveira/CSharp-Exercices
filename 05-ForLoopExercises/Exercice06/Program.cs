d0e8ffdabc14e934615dcb965b09fd6280ac8771
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-15 23:49:53 -0300

    resolução do exercicio 06

--- 05-ForLoopExercises/Exercice06/Program.cs
+++ 05-ForLoopExercises/Exercice06/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa em C# Sharp para exibir a tabela de multiplicação de um determinado número inteiro.
 Dados de teste:
 Insira o número (tabela a ser calculada): 15
@@ -8,3 +10,22 @@ Saída esperada :
 ...
 15 X 10 = 150
 */
+
+namespace Exercice06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira o número (tabela a ser calculada): ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Tabela de multiplicação do número {number}:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int result = number * i;
+            Console.WriteLine($"{number} X {i} = {result}");
+        }
+    }
+}
