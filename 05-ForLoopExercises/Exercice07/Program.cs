1d08c8e989b34349b97fe460379174ce0d3c253b
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-18 21:49:09 -0300

    resolução do exercicio 07

--- 05-ForLoopExercises/Exercice07/Program.cs
+++ 05-ForLoopExercises/Exercice07/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa em C# Sharp para exibir a tabuada verticalmente de 1 a n.
 Dados de teste:
 Entrada até o número da tabela começando em 1: 8
@@ -8,3 +10,28 @@ Tabela de multiplicação de 1 a 8
 ...
 1x10 = 10, 2x10 = 20, 3x10 = 30, 4x10 = 40, 5x10 = 50, 6x10 = 60, 7x10 = 70, 8x10 = 80
 */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o número da tabela começando em 1: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Tabela de multiplicação de 1 a {n}");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                int result = i * j;
+                Console.Write($"{j}x{i} = {result}");
+                if (j < n)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
