6152fb4a5b94f6fe04d6f5f9f0368c3aa0b7aa9c
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-19 23:30:58 -0300

    resolução do exercicio 10

--- 05-ForLoopExercises/Exercice10/Program.cs
+++ 05-ForLoopExercises/Exercice10/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa em C# Sharp para exibir um padrão como um triângulo retângulo com um número.
 O padrão como:
 
@@ -7,3 +9,22 @@ O padrão como:
 123
 1234
 */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of lines: ");
+        int lines = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Padrão de triângulo retângulo com números:");
+        for (int i = 1; i <= lines; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+            Console.WriteLine();
+        }
+    }
+}
