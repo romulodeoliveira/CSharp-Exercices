d9177ec568d94c44525b455ebbe8b8b60fafc5ae
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-19 23:29:28 -0300

    resolução do exercicio 09

--- 05-ForLoopExercises/Exercice09/Program.cs
+++ 05-ForLoopExercises/Exercice09/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa em C# Sharp para exibir um triângulo retângulo com um asterisco.
 O padrão como:
 
@@ -7,3 +9,22 @@ O padrão como:
 ***
 ****
 */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o número de linhas: ");
+        int linhas = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Padrão de triângulo retângulo:");
+        for (int i = 1; i <= linhas; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
