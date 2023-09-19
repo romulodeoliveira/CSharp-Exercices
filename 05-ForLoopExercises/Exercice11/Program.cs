747d07a19a4205f1ba8cb63f90b3e2c657d237d9
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-18 23:01:07 -0300

    resolução do exercicio 11

--- 05-ForLoopExercises/Exercice11/Program.cs
+++ 05-ForLoopExercises/Exercice11/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa em C# Sharp para fazer um padrão como um triângulo retângulo com um número que repete um número em uma linha.
 O padrão como:
 
@@ -7,3 +9,28 @@ O padrão como:
 333
 4444
 */
+
+class Program
+{
+    static void Main()
+    {
+        int lines;
+
+        Console.Write("Digite o número de linhas para o padrão: ");
+        if (int.TryParse(Console.ReadLine(), out lines))
+        {
+            for (int i = 1; i <= lines; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro válido.");
+        }
+    }
+}
