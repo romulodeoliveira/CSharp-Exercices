5297e28421afd12b17beb593dc6a924afa3e515b
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-21 00:05:24 -0300

    resolução do exercicio 12

--- 05-ForLoopExercises/Exercice12/Program.cs
+++ 05-ForLoopExercises/Exercice12/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using System;
+
+/*
 Escreva um programa C# Sharp para fazer um padrão como um triângulo de ângulo reto com o número aumentado em 1.
 O padrão como:
 
@@ -7,3 +9,30 @@ O padrão como:
 4 5 6
 7 8 9 10
 */
+
+class Program
+{
+    static void Main()
+    {
+        int lines;
+        int number = 1;
+
+        Console.Write("Digite o número de linhas para o padrão: ");
+        if (int.TryParse(Console.ReadLine(), out lines))
+        {
+            for (int i = 1; i <= lines; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(number + " ");
+                    number++;
+                }
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro válido.");
+        }
+    }
+}
