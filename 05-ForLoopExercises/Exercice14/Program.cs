2fbb9eef33df8c5a9874f8dd28834119ba278f7d
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-14 00:08:17 -0300

    resolução do exercicio 14

--- 05-ForLoopExercises/Exercice14/Program.cs
+++ 05-ForLoopExercises/Exercice14/Program.cs
@@ -6,3 +6,28 @@ Escreva um programa em C# Sharp para fazer um padrão como uma pirâmide com um 
  * * *
 * * * *
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 4;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
