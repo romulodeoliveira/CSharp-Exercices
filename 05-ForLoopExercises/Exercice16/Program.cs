f9762112f978072eb3050625dcf7545ca119b5fb
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-16 00:01:18 -0300

    resolução do exercicio 16

--- 05-ForLoopExercises/Exercice16/Program.cs
+++ 05-ForLoopExercises/Exercice16/Program.cs
@@ -6,3 +6,31 @@ Saída esperada :
 Os números pares são: 2 4 6 8 10
 A soma do número natural par até 5 termos: 30
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Número de termos de entrada: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int soma = 0;
+        Console.Write("Os números pares são: ");
+        for (int i = 1; i <= n; i++)
+        {
+            int numeroPar = 2 * i;
+            soma += numeroPar;
+
+            Console.Write(numeroPar);
+            if (i < n)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("A soma do número natural par até " + n + " termos: " + soma);
+    }
+}
