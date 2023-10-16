e99ef489378e25b3b94668dc001417cc3fa0519e
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-15 23:54:13 -0300

    resolução do exercicio 15

--- 05-ForLoopExercises/Exercice15/Program.cs
+++ 05-ForLoopExercises/Exercice15/Program.cs
@@ -5,3 +5,32 @@ Insira o número: 5
 Saída esperada :
 O fatorial de 5 é: 120
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        long fatorial = CalcularFatorial(numero);
+
+        Console.WriteLine($"O fatorial de {numero} é: {fatorial}");
+    }
+
+    static long CalcularFatorial(int numero)
+    {
+        if (numero == 0)
+        {
+            return 1;
+        }
+        long fatorial = 1;
+        for (int i = 1; i <= numero; i++)
+        {
+            fatorial *= i;
+        }
+        return fatorial;
+    }
+}
