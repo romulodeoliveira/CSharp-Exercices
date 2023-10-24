c3009430c819e52fbf8d4c19f8ff5a55698d6ebb
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-23 21:11:17 -0300

    resolução do exercicio 17

--- 05-ForLoopExercises/Exercice17/Program.cs
+++ 05-ForLoopExercises/Exercice17/Program.cs
@@ -6,3 +6,33 @@ Escreva um programa em C# Sharp para fazer um padrão como uma pirâmide com um 
  3 3 3
 4 4 4 4
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int linhas;
+
+        Console.Write("Digite o número de linhas para a pirâmide: ");
+        linhas = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= linhas; i++)
+        {
+            for (int j = 1; j <= linhas - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.ReadLine(); 
+    }
+}
