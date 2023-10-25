0d8009288e85ea7b8937c64ab235ac62bc96245e
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-24 22:19:11 -0300

    resolução do exercicio 18

--- 05-ForLoopExercises/Exercice18/Program.cs
+++ 05-ForLoopExercises/Exercice18/Program.cs
@@ -8,3 +8,39 @@ a soma = -0,415873
 Número de termos = 5
 valor de x = 2,000000
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor de x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o número de termos: ");
+        int numberOfTerms = Convert.ToInt32(Console.ReadLine());
+
+        double sum = CalculateSumOfSeries(x, numberOfTerms);
+
+        Console.WriteLine($"A soma = {sum:F6}");
+        Console.WriteLine($"Número de termos = {numberOfTerms}");
+        Console.WriteLine($"Valor de x = {x:F6}");
+
+        Console.ReadLine();
+    }
+
+    static double CalculateSumOfSeries(double x, int numberOfTerms)
+    {
+        double sum = 1.0;
+        double term = 1.0;
+
+        for (int i = 1; i < numberOfTerms; i++)
+        {
+            term *= -1 * (x * x) / ((2 * i) * (2 * i + 1));
+            sum += term;
+        }
+
+        return sum;
+    }
+}
