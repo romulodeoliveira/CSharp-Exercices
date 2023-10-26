e8aca99604c21eb95d0f8be05e5d83e850481f32
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-25 21:27:10 -0300

    resolução do exercicio 19

--- 05-ForLoopExercises/Exercice19/Program.cs
+++ 05-ForLoopExercises/Exercice19/Program.cs
@@ -7,3 +7,53 @@ Saída esperada :
 1/1 + 1/2 + 1/3 + 1 /4 + 1/5 +
 Soma de Séries até 5 termos : 2.283334
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of terms: ");
+        int numberOfTerms = Convert.ToInt32(Console.ReadLine());
+
+        DisplayHarmonicSeries(numberOfTerms);
+
+        double sum = CalculateSumHarmonicSeries(numberOfTerms);
+
+        Console.WriteLine($"Soma de Séries até {numberOfTerms} termos: {sum:F6}");
+
+        Console.ReadLine();
+    }
+
+    static void DisplayHarmonicSeries(int n)
+    {
+        Console.Write("Série Harmônica: ");
+        double term = 0.0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            term = 1.0 / i;
+            Console.Write($"{term:F5} ");
+
+            if (i < n)
+            {
+                Console.Write("+ ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    static double CalculateSumHarmonicSeries(int n)
+    {
+        double sum = 0.0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum += 1.0 / i;
+        }
+
+        return sum;
+    }
+}
