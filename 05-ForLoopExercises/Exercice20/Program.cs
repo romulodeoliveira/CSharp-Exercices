0e8b2ed3535317ebb61d047943d4cf6befdbe678
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-26 21:33:05 -0300

    resolução do exercicio 20

--- 05-ForLoopExercises/Exercice20/Program.cs
+++ 05-ForLoopExercises/Exercice20/Program.cs
@@ -5,3 +5,38 @@ Escreva um programa em C# Sharp para exibir o padrão como pirâmide usando um a
  ***
 *****
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o número de linhas para a pirâmide: ");
+        int numberOfLines = Convert.ToInt32(Console.ReadLine());
+
+        DisplayPyramidPattern(numberOfLines);
+
+        Console.ReadLine();
+    }
+
+    static void DisplayPyramidPattern(int n)
+    {
+        Console.WriteLine("Padrão de Pirâmide:");
+
+        for (int i = 1; i <= n; i += 2)
+        {
+            for (int j = 0; j < (n - i) / 2; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 0; k < i; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
