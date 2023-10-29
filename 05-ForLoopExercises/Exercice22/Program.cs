90ebe04a84970af83cbcc6da5cd16f846b966b1c
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-28 21:17:17 -0300

    resolução do exercicio 22

--- 05-ForLoopExercises/Exercice22/Program.cs
+++ 05-ForLoopExercises/Exercice22/Program.cs
@@ -7,3 +7,36 @@ Escreva um programa em C# Sharp para imprimir o Triângulo de Floyd.
 0101
 10101
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o número de linhas para o Triângulo de Floyd: ");
+        int numberOfRows = Convert.ToInt32(Console.ReadLine());
+
+        DisplayFloydTriangle(numberOfRows);
+
+        Console.ReadLine();
+    }
+
+    static void DisplayFloydTriangle(int n)
+    {
+        Console.WriteLine("Triângulo de Floyd:");
+
+        int num = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write($"{num % 2}");
+                num++;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
