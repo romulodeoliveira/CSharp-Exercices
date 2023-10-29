c0b615f4d37489db3a2c9d3e39f5bda5626177a3
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-28 21:23:11 -0300

    resolução do exercicio 24

--- 05-ForLoopExercises/Exercice23/Program.cs
+++ 05-ForLoopExercises/Exercice23/Program.cs
@@ -8,3 +8,39 @@ A soma é: 16,375000
 Número de termos = 5
 O valor de x = 3,000000
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o valor de x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Insira o número de termos: ");
+        int numberOfTerms = Convert.ToInt32(Console.ReadLine());
+
+        double sum = CalculateSeriesSum(x, numberOfTerms);
+
+        Console.WriteLine($"A soma é: {sum:F6}");
+        Console.WriteLine($"Número de termos = {numberOfTerms}");
+        Console.WriteLine($"O valor de x = {x:F6}");
+
+        Console.ReadLine();
+    }
+
+    static double CalculateSeriesSum(double x, int n)
+    {
+        double sum = 1.0;
+        double term = 1.0;
+
+        for (int i = 1; i < n; i++)
+        {
+            term *= x / i;
+            sum += term;
+        }
+
+        return sum;
+    }
+}
--- 05-ForLoopExercises/Exercice24/Program.cs
+++ 05-ForLoopExercises/Exercice24/Program.cs
@@ -7,3 +7,39 @@ A soma = 410
 Número de termos = 5
 O valor de x = 2
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o valor de x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Insira o número de termos: ");
+        int numberOfTerms = Convert.ToInt32(Console.ReadLine());
+
+        double sum = CalculateSeriesSum(x, numberOfTerms);
+
+        Console.WriteLine($"A soma = {sum}");
+        Console.WriteLine($"Número de termos = {numberOfTerms}");
+        Console.WriteLine($"O valor de x = {x}");
+
+        Console.ReadLine();
+    }
+
+    static double CalculateSeriesSum(double x, int n)
+    {
+        double sum = 0;
+        int power = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum += Math.Pow(x, power) * (i % 2 == 1 ? 1 : -1);
+            power += 2;
+        }
+
+        return sum;
+    }
+}
