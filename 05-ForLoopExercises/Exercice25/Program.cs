321525b38f7dd3694dfe97c9206b093b84037fab
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-29 21:12:03 -0300

    resolução do exercicio 25

--- 05-ForLoopExercises/Exercice25/Program.cs
+++ 05-ForLoopExercises/Exercice25/Program.cs
@@ -7,3 +7,46 @@ Saída esperada :
 O quadrado natural até 5 termos é: 1 4 9 16 25
 A soma do quadrado do número natural até 5 termos = 55
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número de termos: ");
+        int numberOfTerms = Convert.ToInt32(Console.ReadLine());
+
+        DisplayNaturalSquares(numberOfTerms);
+
+        int sum = CalculateSquareSum(numberOfTerms);
+
+        Console.WriteLine($"A soma do quadrado do número natural até {numberOfTerms} termos = {sum}");
+
+        Console.ReadLine();
+    }
+
+    static void DisplayNaturalSquares(int n)
+    {
+        Console.Write("O quadrado natural até {0} termos é: ", n);
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write($"{i * i} ");
+        }
+
+        Console.WriteLine();
+    }
+
+    static int CalculateSquareSum(int n)
+    {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i * i;
+        }
+
+        return sum;
+    }
+}
