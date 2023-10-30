89fcd9c5276ded56ffb4bdb3846551eef803403f
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-29 21:21:12 -0300

    resolução do exercicio 26

--- 05-ForLoopExercises/Exercice26/Program.cs
+++ 05-ForLoopExercises/Exercice26/Program.cs
@@ -6,3 +6,56 @@ Saída esperada :
 1 + 11 + 111 + 1111 + 11111 +
 A soma é: 12345
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número de termos: ");
+        int numberOfTerms = Convert.ToInt32(Console.ReadLine());
+
+        DisplaySeriesTerms(numberOfTerms);
+
+        int sum = CalculateSeriesSum(numberOfTerms);
+
+        Console.WriteLine($"A soma é: {sum}");
+
+        Console.ReadLine();
+    }
+
+    static void DisplaySeriesTerms(int n)
+    {
+        Console.Write("Série: ");
+
+        int term = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            term = term * 10 + 1;
+            Console.Write($"{term}");
+
+            if (i < n)
+            {
+                Console.Write(" + ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    static int CalculateSeriesSum(int n)
+    {
+        int sum = 0;
+        int term = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            term = term * 10 + 1;
+            sum += term;
+        }
+
+        return sum;
+    }
+}
