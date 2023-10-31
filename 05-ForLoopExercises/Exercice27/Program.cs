51fe61323c8a0b5842fe9ecc6d24fddd667d938c
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-30 21:01:03 -0300

    resolução do exercicio 27

--- 05-ForLoopExercises/Exercice27/Program.cs
+++ 05-ForLoopExercises/Exercice27/Program.cs
@@ -7,3 +7,59 @@ O divisor positivo: 1 2 4 7 8 14 28
 A soma do divisor é: 64
 Portanto, o número não é perfeito.
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        DisplayDivisors(number);
+
+        bool isPerfect = CheckPerfectNumber(number);
+
+        if (isPerfect)
+        {
+            Console.WriteLine("Portanto, o número é perfeito.");
+        }
+        else
+        {
+            Console.WriteLine("Portanto, o número não é perfeito.");
+        }
+
+        Console.ReadLine();
+    }
+
+    static void DisplayDivisors(int n)
+    {
+        Console.Write("Os divisores positivos são: ");
+
+        for (int i = 1; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    static bool CheckPerfectNumber(int n)
+    {
+        int sum = 0;
+
+        for (int i = 1; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                sum += i;
+            }
+        }
+
+        return sum == n;
+    }
+}
