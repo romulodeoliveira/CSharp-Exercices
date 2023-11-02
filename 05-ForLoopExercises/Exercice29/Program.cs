7c4f92824f2a2bcc3ae697f51814f39251446094
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-01 21:17:47 -0300

    resolução do exercicio 29

--- 05-ForLoopExercises/Exercice29/Program.cs
+++ 05-ForLoopExercises/Exercice29/Program.cs
@@ -5,3 +5,41 @@ Insira um número: 153
 Saída esperada :
 153 é um número de Armstrong.
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira um número: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsArmstrongNumber(number))
+        {
+            Console.WriteLine($"{number} é um número de Armstrong.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} não é um número de Armstrong.");
+        }
+
+        Console.ReadLine();
+    }
+
+    static bool IsArmstrongNumber(int num)
+    {
+        int originalNumber = num;
+        int result = 0;
+        int n = num.ToString().Length;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+            result += (int)Math.Pow(digit, n);
+            num /= 10;
+        }
+
+        return result == originalNumber;
+    }
+}
