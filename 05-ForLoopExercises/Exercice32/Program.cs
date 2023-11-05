d556eb5d0de9bc2e46f4fdfda0cd6f5337d89eb7
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-04 23:47:13 -0300

    resolução do exercicio 32

--- 05-ForLoopExercises/Exercice32/Program.cs
+++ 05-ForLoopExercises/Exercice32/Program.cs
@@ -5,3 +5,43 @@ Insira um número: 13
 Saída esperada :
 13 é um número primo.
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  Console.Write("Insira um número: ");
+  int number = int.Parse(Console.ReadLine());
+
+  if (IsPrime(number))
+  {
+   Console.WriteLine($"{number} é um número primo.");
+  }
+  else
+  {
+   Console.WriteLine($"{number} não é um número primo.");
+  }
+
+  Console.ReadLine();
+ }
+
+ static bool IsPrime(int num)
+ {
+  if (num <= 1)
+  {
+   return false;
+  }
+
+  for (int i = 2; i <= Math.Sqrt(num); i++)
+  {
+   if (num % i == 0)
+   {
+    return false;
+   }
+  }
+
+  return true;
+ }
+}
