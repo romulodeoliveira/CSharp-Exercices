452f86b6d5a8f809b6a155c77e839a614d5524e1
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-05 00:07:46 -0300

    resolução do exercicio 33

--- 05-ForLoopExercises/Exercice33/Program.cs
+++ 05-ForLoopExercises/Exercice33/Program.cs
@@ -6,3 +6,40 @@ Saída esperada :
 
 1 1 1 1 2 1 1 3 3 1 1 4 6 4 1
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  Console.Write("Número de linhas de entrada: ");
+  int numRows = int.Parse(Console.ReadLine());
+
+  PrintPascalTriangle(numRows);
+
+  Console.ReadLine(); // Para manter a janela do console aberta
+ }
+
+ static void PrintPascalTriangle(int numRows)
+ {
+  for (int i = 0; i < numRows; i++)
+  {
+   int num = 1;
+
+   // Imprime espaços em branco à esquerda
+   for (int j = 0; j < numRows - i; j++)
+   {
+    Console.Write(" ");
+   }
+
+   for (int j = 0; j <= i; j++)
+   {
+    Console.Write(num + " ");
+    num = num * (i - j) / (j + 1);
+   }
+
+   Console.WriteLine();
+  }
+ }
+}
