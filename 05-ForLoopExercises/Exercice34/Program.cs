653c6c66a87f97ed0b36e55154aa44c099772afb
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-06 22:33:18 -0300

    resolução do exercicio 34

--- 05-ForLoopExercises/Exercice34/Program.cs
+++ 05-ForLoopExercises/Exercice34/Program.cs
@@ -7,3 +7,39 @@ Saída esperada :
 Os números primos entre 1 e 50 são:
 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Número de linhas de entrada: ");
+        int numRows = int.Parse(Console.ReadLine());
+
+        PrintPascalTriangle(numRows);
+
+        Console.ReadLine();
+    }
+
+    static void PrintPascalTriangle(int numRows)
+    {
+        for (int i = 0; i < numRows; i++)
+        {
+            int num = 1;
+
+            for (int j = 0; j < numRows - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(num + " ");
+                num = num * (i - j) / (j + 1);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
