ce985b3682f615a063ff5a80d8b5c4b422d8c524
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-07 23:11:56 -0300

    resolução do exercicio 35

--- 05-ForLoopExercises/Exercice35/Program.cs
+++ 05-ForLoopExercises/Exercice35/Program.cs
@@ -7,3 +7,33 @@ Saída esperada :
 Aqui está a série Fibonacci até 10 termos:
 0 1 1 2 3 5 8 13 21 34
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número de termos a serem exibidos: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Aqui está a série Fibonacci até {n} termos:");
+        PrintFibonacciSeries(n);
+
+        Console.ReadLine();
+    }
+
+    static void PrintFibonacciSeries(int n)
+    {
+        int firstTerm = 0, secondTerm = 1, nextTerm;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(firstTerm + " ");
+
+            nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
