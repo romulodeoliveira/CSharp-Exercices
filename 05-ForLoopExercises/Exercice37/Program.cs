db14418a429caee293e36cacd8bb6b82e904edaf
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-08 23:03:11 -0300

    resolução do exercicio 37

--- 05-ForLoopExercises/Exercice37/Program.cs
+++ 05-ForLoopExercises/Exercice37/Program.cs
@@ -5,3 +5,34 @@ Insira um número: 12345
 Saída esperada :
 O número na ordem inversa é: 54321
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira um número: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int reversedNumber = ReverseNumber(number);
+
+        Console.WriteLine($"O número na ordem inversa é: {reversedNumber}");
+
+        Console.ReadLine();
+    }
+
+    static int ReverseNumber(int num)
+    {
+        int reversedNumber = 0;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+            reversedNumber = (reversedNumber * 10) + digit;
+            num /= 10;
+        }
+
+        return reversedNumber;
+    }
+}
