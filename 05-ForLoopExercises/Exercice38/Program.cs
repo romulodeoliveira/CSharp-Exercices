06b03a3a417810d20d7ef4ebc1da5dbac3a928b8
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-09 21:01:06 -0300

    resolução do exercicio 38

--- 05-ForLoopExercises/Exercice38/Program.cs
+++ 05-ForLoopExercises/Exercice38/Program.cs
@@ -5,3 +5,40 @@ Insira um número: 121
 Saída esperada :
 121 é um número palíndromo.
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira um número: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsPalindrome(number))
+        {
+            Console.WriteLine($"{number} é um número palíndromo.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} não é um número palíndromo.");
+        }
+
+        Console.ReadLine();
+    }
+
+    static bool IsPalindrome(int num)
+    {
+        int originalNumber = num;
+        int reversedNumber = 0;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+            reversedNumber = (reversedNumber * 10) + digit;
+            num /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+}
