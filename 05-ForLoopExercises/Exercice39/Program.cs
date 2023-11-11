2645f7ecf77849526ca6af9c813425cc15dc91f6
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-10 21:57:11 -0300

    resolução do exercicio 39

--- 05-ForLoopExercises/Exercice39/Program.cs
+++ 05-ForLoopExercises/Exercice39/Program.cs
@@ -5,3 +5,28 @@ Números entre 100 e 200, divisíveis por 9:
 108 117 126 135 144 153 162 171 180 189 198
 A soma : 1683
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números entre 100 e 200, divisíveis por 9:");
+
+        int sum = 0;
+
+        for (int i = 100; i <= 200; i++)
+        {
+            if (i % 9 == 0)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+        }
+
+        Console.WriteLine($"\nA soma: {sum}");
+
+        Console.ReadLine();
+    }
+}
