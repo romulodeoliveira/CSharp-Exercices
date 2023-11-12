3243e770ce284dd6ee0dc240b3e903e54c401aed
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-11 21:43:46 -0300

    resolução do exercicio 40

--- 05-ForLoopExercises/Exercice40/Program.cs
+++ 05-ForLoopExercises/Exercice40/Program.cs
@@ -6,3 +6,39 @@ ABA
 ABCBA
 ABCDCBA
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        char startChar = 'A';
+        char endChar;
+
+        Console.Write("Insira o último caractere (em maiúscula): ");
+        endChar = char.Parse(Console.ReadLine());
+
+        PrintAlphabetPattern(startChar, endChar);
+
+        Console.ReadLine();
+    }
+
+    static void PrintAlphabetPattern(char startChar, char endChar)
+    {
+        for (char i = startChar; i <= endChar; i++)
+        {
+            for (char j = startChar; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+
+            for (char j = (char)(i - 1); j >= startChar; j--)
+            {
+                Console.Write(j);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
