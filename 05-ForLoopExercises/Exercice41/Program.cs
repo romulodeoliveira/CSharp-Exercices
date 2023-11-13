47c986398ec19b7997d99de5c5cc27983a3e076a
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-12 22:02:48 -0300

    resolução do exercicio 41

--- 05-ForLoopExercises/Exercice41/Program.cs
+++ 05-ForLoopExercises/Exercice41/Program.cs
@@ -5,3 +5,39 @@ Digite um número para converter: 25
 Saída esperada :
 O binário de 25 é 11001.
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número para converter: ");
+        int decimalNumber = int.Parse(Console.ReadLine());
+
+        string binaryRepresentation = ConvertDecimalToBinary(decimalNumber);
+
+        Console.WriteLine($"O binário de {decimalNumber} é {binaryRepresentation}");
+
+        Console.ReadLine();
+    }
+
+    static string ConvertDecimalToBinary(int decimalNumber)
+    {
+        if (decimalNumber == 0)
+        {
+            return "0";
+        }
+
+        string binary = "";
+
+        while (decimalNumber > 0)
+        {
+            int remainder = decimalNumber % 2;
+            binary = remainder + binary;
+            decimalNumber /= 2;
+        }
+
+        return binary;
+    }
+}
