b740f40fa0f761fa830f8ad11fb6f6b855dcdf89
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-13 21:39:13 -0300

    resolução do exercicio 42

--- 05-ForLoopExercises/Exercice42/Program.cs
+++ 05-ForLoopExercises/Exercice42/Program.cs
@@ -6,3 +6,36 @@ Saída esperada :
 O número binário: 1010101
 O número decimal equivalente: 85
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira um número binário: ");
+        string binaryNumber = Console.ReadLine();
+
+        int decimalEquivalent = ConvertBinaryToDecimal(binaryNumber);
+
+        Console.WriteLine($"O número binário: {binaryNumber}");
+        Console.WriteLine($"O número decimal equivalente: {decimalEquivalent}");
+
+        Console.ReadLine();
+    }
+
+    static int ConvertBinaryToDecimal(string binaryNumber)
+    {
+        int decimalEquivalent = 0;
+        int binaryLength = binaryNumber.Length;
+
+        for (int i = 0; i < binaryLength; i++)
+        {
+            char binaryDigit = binaryNumber[binaryLength - 1 - i];
+            int digitValue = (binaryDigit == '1') ? 1 : 0;
+            decimalEquivalent += digitValue * (int)Math.Pow(2, i);
+        }
+
+        return decimalEquivalent;
+    }
+}
