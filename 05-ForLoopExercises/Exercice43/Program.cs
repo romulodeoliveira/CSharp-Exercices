b5f024d78de37b35ef2b977f0047c0b64302c6d0
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-14 21:27:57 -0300

    resolução do exercicio 43

--- 05-ForLoopExercises/Exercice43/Program.cs
+++ 05-ForLoopExercises/Exercice43/Program.cs
@@ -6,3 +6,35 @@ Insira o 2º número para HCF: 28
 Saída esperada :
 HCF de 24 e 28 é: 4
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o 1º número para HCF: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Insira o 2º número para HCF: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int hcf = FindHCF(num1, num2);
+
+        Console.WriteLine($"HCF de {num1} e {num2} é: {hcf}");
+
+        Console.ReadLine();
+    }
+
+    static int FindHCF(int num1, int num2)
+    {
+        while (num2 != 0)
+        {
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+
+        return num1;
+    }
+}
