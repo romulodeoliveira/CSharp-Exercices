dcf72bbbe00477fa304928bf0e3fca125a1760aa
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-15 02:04:53 -0300

    resolução do exercicio 28

--- 05-ForLoopExercises/Exercice28/Program.cs
+++ 05-ForLoopExercises/Exercice28/Program.cs
@@ -6,3 +6,44 @@ Insira o intervalo final do número: 50
 Saída esperada :
 Os números perfeitos dentro do intervalo fornecido: 6 28
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o intervalo inicial: ");
+        int start = int.Parse(Console.ReadLine());
+
+        Console.Write("Insira o intervalo final: ");
+        int end = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Os números perfeitos dentro do intervalo fornecido:");
+
+        for (int num = start; num <= end; num++)
+        {
+            if (IsPerfectNumber(num))
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+        Console.ReadLine();
+    }
+
+    static bool IsPerfectNumber(int num)
+    {
+        int sum = 0;
+
+        for (int i = 1; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                sum += i;
+            }
+        }
+
+        return sum == num;
+    }
+}
