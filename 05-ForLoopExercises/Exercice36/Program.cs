fe73101b53d41d94efba29218019e06e741edcc7
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-15 19:35:36 -0300

    resolução do exercicio 36

--- 05-ForLoopExercises/Exercice36/Program.cs
+++ 05-ForLoopExercises/Exercice36/Program.cs
@@ -5,3 +5,41 @@ Escreva um programa em C# Sharp para exibir tal padrão para n número de linhas
  121
 12321
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número de linhas: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintPattern(n);
+
+        Console.ReadLine();
+    }
+
+    static void PrintPattern(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+
+            for (int j = i - 1; j >= 1; j--)
+            {
+                Console.Write(j);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
