139cc2292a57bd6db6c149b4777f9a381005904c
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-16 23:19:11 -0300

    resolução do exercicio 44

--- 05-ForLoopExercises/Exercice44/Program.cs
+++ 05-ForLoopExercises/Exercice44/Program.cs
@@ -6,3 +6,43 @@ Insira o 2º número para LCM: 20
 Saída esperada :
 O LCM de 15 e 20 é: 60
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o 1º número para LCM: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Insira o 2º número para LCM: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int lcm = FindLCM(num1, num2);
+
+        Console.WriteLine($"O LCM de {num1} e {num2} é: {lcm}");
+
+        Console.ReadLine();
+    }
+
+    static int FindLCM(int num1, int num2)
+    {
+        int hcf = FindHCF(num1, num2);
+        int lcm = (num1 * num2) / hcf;
+
+        return lcm;
+    }
+
+    static int FindHCF(int num1, int num2)
+    {
+        while (num2 != 0)
+        {
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+
+        return num1;
+    }
+}
