a927c082071c1ef7a0476ddde37b1d112adcc5cc
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-19 21:16:56 -0300

    resolução do exercicio 46

--- 05-ForLoopExercises/Exercice46/Program.cs
+++ 05-ForLoopExercises/Exercice46/Program.cs
@@ -6,3 +6,34 @@ Saída esperada :
 O número binário: 1010100
 O número decimal equivalente é: 84
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número binário: ");
+        string binaryNumber = Console.ReadLine();
+
+        int decimalNumber = BinaryToDecimal(binaryNumber);
+
+        Console.WriteLine($"O número binário: {binaryNumber}");
+        Console.WriteLine($"O número decimal equivalente é: {decimalNumber}");
+
+        Console.ReadLine();
+    }
+
+    static int BinaryToDecimal(string binary)
+    {
+        int decimalNumber = 0;
+
+        for (int i = binary.Length - 1, j = 0; i >= 0; i--, j++)
+        {
+            int bit = int.Parse(binary[i].ToString());
+            decimalNumber += bit * (int)Math.Pow(2, j);
+        }
+
+        return decimalNumber;
+    }
+}
