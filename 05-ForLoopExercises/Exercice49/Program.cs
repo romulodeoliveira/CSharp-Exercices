cec3b74be71d57ec0e1ef41227c610455e5365b9
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-22 21:33:21 -0300

    resolução do exercicio 49

--- 05-ForLoopExercises/Exercice49/Program.cs
+++ 05-ForLoopExercises/Exercice49/Program.cs
@@ -8,3 +8,29 @@ Saída esperada :
 A soma da série AP é:
 1 + 5 + 9 + 13 + 17 + 21 + 25 + 29 + 33 + 37 = 190
 */
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int firstTerm = int.Parse(args[0]);
+        int numberOfTerms = int.Parse(args[1]);
+        int commonDifference = int.Parse(args[2]);
+
+        int sum = (firstTerm + lastTerm(firstTerm, numberOfTerms, commonDifference)) / 2;
+
+        Console.WriteLine($"A soma da série AP é:");
+        for (int i = 0; i < numberOfTerms; i++)
+        {
+            Console.Write($" {firstTerm + i * commonDifference}");
+        }
+        Console.WriteLine(" = {sum}");
+    }
+
+    private static int lastTerm(int firstTerm, int numberOfTerms, int commonDifference)
+    {
+        return firstTerm + (numberOfTerms - 1) * commonDifference;
+    }
+}
