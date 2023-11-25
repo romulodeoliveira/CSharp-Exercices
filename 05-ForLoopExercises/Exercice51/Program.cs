29ee7af68e664273e34495790b9f142976f2ee0c
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-24 21:31:47 -0300

    resolução do exercicio 51

--- 05-ForLoopExercises/Exercice51/Program.cs
+++ 05-ForLoopExercises/Exercice51/Program.cs
@@ -6,3 +6,22 @@ Saída esperada :
 O número octal: 745
 O número decimal equivalente: 485
 */
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string octalNumber = Console.ReadLine();
+
+        int decimalNumber = 0;
+        for (int i = octalNumber.Length - 1; i >= 0; i--)
+        {
+            decimalNumber += (octalNumber[i] - '0') * (int)Math.Pow(8, octalNumber.Length - 1 - i);
+        }
+
+        Console.WriteLine($"O número octal: {octalNumber}");
+        Console.WriteLine($"O número decimal equivalente: {decimalNumber}");
+    }
+}
