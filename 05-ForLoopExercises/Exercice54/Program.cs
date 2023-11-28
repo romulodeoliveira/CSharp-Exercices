70125cb227bc8b2fd3c8f0bf2c86f22fe9b05836
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-27 21:36:16 -0300

    resolução do exercicio 54

--- 05-ForLoopExercises/Exercice54/Program.cs
+++ 05-ForLoopExercises/Exercice54/Program.cs
@@ -6,3 +6,32 @@ Saída esperada :
 O número octal: 11
 O número binário equivalente: 1001
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira um número octal: ");
+        string octalInput = Console.ReadLine();
+
+        string binaryOutput = OctalToBinary(octalInput);
+
+        Console.WriteLine($"O número octal: {octalInput}");
+        Console.WriteLine($"O número binário equivalente: {binaryOutput}");
+    }
+
+    static string OctalToBinary(string octalNumber)
+    {
+        string binaryResult = "";
+        foreach (char octalDigit in octalNumber)
+        {
+            int decimalValue = Convert.ToInt32(octalDigit.ToString(), 8);
+            string binaryDigit = Convert.ToString(decimalValue, 2).PadLeft(3, '0');
+            binaryResult += binaryDigit;
+        }
+
+        return binaryResult;
+    }
+}
