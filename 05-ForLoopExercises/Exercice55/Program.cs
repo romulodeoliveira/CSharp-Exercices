e2ac86e15ad2a730842dbc3a6c248efac17008c6
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-28 21:45:57 -0300

    resolução do exercicio 55

--- 05-ForLoopExercises/Exercice55/Program.cs
+++ 05-ForLoopExercises/Exercice55/Program.cs
@@ -5,3 +5,25 @@ Insira qualquer número decimal: 79
 Saída esperada :
 O número hexadecimal equivalente: 4F
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira qualquer número decimal: ");
+        int decimalInput = int.Parse(Console.ReadLine());
+
+        string hexadecimalOutput = DecimalToHexadecimal(decimalInput);
+
+        Console.WriteLine($"O número hexadecimal equivalente: {hexadecimalOutput}");
+    }
+
+    static string DecimalToHexadecimal(int decimalNumber)
+    {
+        string hexadecimalResult = Convert.ToString(decimalNumber, 16).ToUpper();
+
+        return hexadecimalResult;
+    }
+}
