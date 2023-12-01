fd5cc3206d406193d12d7223e28e8d08f2d48a46
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-11-30 21:17:13 -0300

    resolução do exercicio 57

--- 05-ForLoopExercises/Exercice57/Program.cs
+++ 05-ForLoopExercises/Exercice57/Program.cs
@@ -5,3 +5,25 @@ Insira uma string para reverter: Bem-vindo
 Saída esperada :
 A string invertida é: emocleW
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira uma string para reverter: ");
+        string inputString = Console.ReadLine();
+
+        string reversedString = ReverseString(inputString);
+
+        Console.WriteLine($"A string invertida é: {reversedString}");
+    }
+
+    static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
