168b85b15613d26c43ff851c75e17ec10ec1a126
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-01 21:19:53 -0300

    resolução do exercicio 58

--- 05-ForLoopExercises/Exercice58/Program.cs
+++ 05-ForLoopExercises/Exercice58/Program.cs
@@ -1,15 +1,46 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como A com um asterisco.
 
-A string invertida é: Exibe o padrão como 'A' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
-  ***                                                                            
- * *                                                                           
- * *                                                                           
- *****                                                                           
- * *                                                                           
- * *                                                                           
- * *                                                                           
+A string invertida é: Exibe o padrão como 'A' com um asterisco:
+---------------------------------------------
+
+  ***
+ * *
+ * *
+ *****
+ * *
+ * *
+ * *
  * *
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        DisplayAlphabetPatternA();
+    }
+
+    static void DisplayAlphabetPatternA()
+    {
+        Console.WriteLine("Exibe o padrão como 'A' com um asterisco:");
+        
+        for (int i = 1; i <= 7; i++)
+        {
+            for (int j = 1; j <= 7; j++)
+            {
+                if ((i == 1 && j % 3 != 0) || (i > 1 && (j == 1 || j == 7)) || (i == 4 && j % 3 == 0))
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
