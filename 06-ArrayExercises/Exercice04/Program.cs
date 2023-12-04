d0494c41c2f45923f61f63b21ec4f9fb6877cf2b
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-03 22:11:03 -0300

    resolução do exercicio 04

--- 06-ArrayExercises/Exercice04/Program.cs
+++ 06-ArrayExercises/Exercice04/Program.cs
@@ -12,3 +12,44 @@ Os elementos armazenados na primeira matriz são:
 Os elementos copiados no segundo array são:
 15 10 12
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número de elementos a serem armazenados na matriz: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arrayOriginal = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Elemento - {i}: ");
+            arrayOriginal[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int[] arrayCopia = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            arrayCopia[i] = arrayOriginal[i];
+        }
+
+        Console.WriteLine("Os elementos armazenados na primeira matriz são:");
+        ExibirArray(arrayOriginal);
+
+        Console.WriteLine("Os elementos copiados no segundo array são:");
+        ExibirArray(arrayCopia);
+    }
+
+    static void ExibirArray(int[] array)
+    {
+        foreach (int elemento in array)
+        {
+            Console.Write($"{elemento} ");
+        }
+        Console.WriteLine();
+    }
+}
