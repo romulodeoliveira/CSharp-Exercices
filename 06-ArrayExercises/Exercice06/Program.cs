96ee5fc34d6c2cd76b7043f4927afcdb86e73926
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-05 23:11:17 -0300

    resolução do exercicio 06

--- 06-ArrayExercises/Exercice06/Program.cs
+++ 06-ArrayExercises/Exercice06/Program.cs
@@ -10,3 +10,51 @@ Saída esperada :
 Os elementos exclusivos encontrados na matriz são:
 5
 */
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número de elementos a serem armazenados na matriz: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Elemento - {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int[] elementosUnicos = ObterElementosUnicos(array);
+
+        Console.WriteLine("Os elementos exclusivos encontrados na matriz são:");
+        ExibirArray(elementosUnicos);
+    }
+
+    static int[] ObterElementosUnicos(int[] array)
+    {
+        HashSet<int> conjunto = new HashSet<int>();
+
+        foreach (int elemento in array)
+        {
+            conjunto.Add(elemento);
+        }
+
+        int[] elementosUnicos = new int[conjunto.Count];
+        conjunto.CopyTo(elementosUnicos);
+
+        return elementosUnicos;
+    }
+
+    static void ExibirArray(int[] array)
+    {
+        foreach (int elemento in array)
+        {
+            Console.WriteLine(elemento);
+        }
+    }
+}
