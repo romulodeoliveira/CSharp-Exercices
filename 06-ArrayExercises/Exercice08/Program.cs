ec354d85efe671d6795d882ebc5cf8b8d5a78236
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-07 21:04:56 -0300

    resolução do exercicio 08

--- 06-ArrayExercises/Exercice08/Program.cs
+++ 06-ArrayExercises/Exercice08/Program.cs
@@ -12,3 +12,55 @@ Frequência de todos os elementos da matriz:
 ocorre 12 1 vez
 ocorre 43 1 vez
 */
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número de elementos a serem armazenados na matriz: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Elemento - {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Dictionary<int, int> frequencia = ContarFrequencia(array);
+
+        Console.WriteLine("\nFrequência de todos os elementos da matriz:");
+        ExibirFrequencia(frequencia);
+    }
+
+    static Dictionary<int, int> ContarFrequencia(int[] array)
+    {
+        Dictionary<int, int> frequencia = new Dictionary<int, int>();
+
+        foreach (int elemento in array)
+        {
+            if (frequencia.ContainsKey(elemento))
+            {
+                frequencia[elemento]++;
+            }
+            else
+            {
+                frequencia[elemento] = 1;
+            }
+        }
+
+        return frequencia;
+    }
+
+    static void ExibirFrequencia(Dictionary<int, int> frequencia)
+    {
+        foreach (var par in frequencia)
+        {
+            Console.WriteLine($"{par.Key} ocorre {par.Value} vez{(par.Value == 1 ? "" : "es")}");
+        }
+    }
+}
