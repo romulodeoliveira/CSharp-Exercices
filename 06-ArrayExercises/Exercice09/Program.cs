0adc7cb3782f8227f722061f9a943998cb94fe69
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-08 21:11:51 -0300

    resolução do exercicio 09

--- 06-ArrayExercises/Exercice09/Program.cs
+++ 06-ArrayExercises/Exercice09/Program.cs
@@ -10,3 +10,59 @@ Saída esperada :
 O elemento máximo é: 45 O
 elemento mínimo é: 21
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número de elementos a serem armazenados na matriz: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Elemento - {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int maximo = EncontrarMaximo(array);
+        int minimo = EncontrarMinimo(array);
+
+        // Exibir os resultados
+        Console.WriteLine($"\nO elemento máximo é: {maximo}");
+        Console.WriteLine($"O elemento mínimo é: {minimo}");
+    }
+
+    static int EncontrarMaximo(int[] array)
+    {
+        int maximo = array[0];
+
+        foreach (int elemento in array)
+        {
+            if (elemento > maximo)
+            {
+                maximo = elemento;
+            }
+        }
+
+        return maximo;
+    }
+
+    static int EncontrarMinimo(int[] array)
+    {
+        int minimo = array[0];
+
+        foreach (int elemento in array)
+        {
+            if (elemento < minimo)
+            {
+                minimo = elemento;
+            }
+        }
+
+        return minimo;
+    }
+}
