4fc86ff5437e0e5be4194f3afd40e7a67eafa59b
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-11 21:03:49 -0300

    resolução do exercicio 12

--- 06-ArrayExercises/Exercice12/Program.cs
+++ 06-ArrayExercises/Exercice12/Program.cs
@@ -10,3 +10,41 @@ Saída esperada :
 Elementos da matriz em ordem decrescente:
 9 5 1
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o tamanho da matriz: ");
+        int tamanho = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[tamanho];
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.Write($"Elemento - {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        OrdenarArrayDecrescente(array);
+
+        Console.WriteLine("\nElementos da matriz em ordem decrescente:");
+        ExibirArray(array);
+    }
+
+    static void OrdenarArrayDecrescente(int[] array)
+    {
+        Array.Sort(array, (a, b) => b.CompareTo(a));
+    }
+
+    static void ExibirArray(int[] array)
+    {
+        foreach (int elemento in array)
+        {
+            Console.Write($"{elemento} ");
+        }
+        Console.WriteLine();
+    }
+}
