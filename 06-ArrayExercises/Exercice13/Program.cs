8cebed743460470df62ae797d12deb6ec6edab18
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-12 21:55:13 -0300

    resolução do exercicio 13

--- 06-ArrayExercises/Exercice13/Program.cs
+++ 06-ArrayExercises/Exercice13/Program.cs
@@ -13,3 +13,59 @@ A lista de matriz existente é:
 Depois de inserir a lista é:
 5 7 8 9
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o tamanho da matriz: ");
+        int tamanho = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[tamanho];
+        Console.WriteLine("Insira os elementos na matriz em ordem crescente:");
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.Write($"elemento - {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("Insira o valor a ser inserido: ");
+        int valorInserir = Convert.ToInt32(Console.ReadLine());
+
+        InserirValorOrdenado(array, valorInserir);
+
+        Console.WriteLine("\nA lista de matriz existente é:");
+        ExibirArray(array);
+    }
+
+    static void InserirValorOrdenado(int[] array, int valor)
+    {
+        int indiceInserir = Array.BinarySearch(array, valor);
+
+        if (indiceInserir < 0)
+        {
+            indiceInserir = ~indiceInserir;
+        }
+
+        Array.Resize(ref array, array.Length + 1);
+
+        for (int i = array.Length - 1; i > indiceInserir; i--)
+        {
+            array[i] = array[i - 1];
+        }
+
+        array[indiceInserir] = valor;
+    }
+
+    static void ExibirArray(int[] array)
+    {
+        foreach (int elemento in array)
+        {
+            Console.Write($"{elemento} ");
+        }
+        Console.WriteLine();
+    }
+}
