43503ad723ae39374e99e24332274506b978b6dc
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-13 21:41:27 -0300

    resolução do exercicio 14

--- 06-ArrayExercises/Exercice14/Program.cs
+++ 06-ArrayExercises/Exercice14/Program.cs
@@ -15,3 +15,55 @@ A lista atual do array:
 Após inserir o elemento a nova lista é:
 1 5 8 7 10
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o tamanho da matriz: ");
+        int tamanho = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[tamanho];
+        Console.WriteLine("Insira os elementos na matriz em ordem crescente:");
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.Write($"elemento - {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("Insira o valor a ser inserido: ");
+        int valorInserir = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Insira a posição onde o valor será inserido: ");
+        int posicaoInserir = Convert.ToInt32(Console.ReadLine());
+
+        InserirValorNaPosicao(array, valorInserir, posicaoInserir);
+
+        Console.WriteLine("\nA lista atual do array:");
+        ExibirArray(array);
+    }
+
+    static void InserirValorNaPosicao(int[] array, int valor, int posicao)
+    {
+        Array.Resize(ref array, array.Length + 1);
+
+        for (int i = array.Length - 1; i > posicao; i--)
+        {
+            array[i] = array[i - 1];
+        }
+
+        array[posicao] = valor;
+    }
+
+    static void ExibirArray(int[] array)
+    {
+        foreach (int elemento in array)
+        {
+            Console.Write($"{elemento} ");
+        }
+        Console.WriteLine();
+    }
+}
