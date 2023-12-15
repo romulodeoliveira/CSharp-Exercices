745feb9849a15f34b1a7284f1e3b476edddca40b
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-14 21:39:55 -0300

    resolução do exercicio 15

--- 06-ArrayExercises/Exercice15/Program.cs
+++ 06-ArrayExercises/Exercice15/Program.cs
@@ -12,3 +12,57 @@ Insira a posição para onde deletar: 3
 Saída Esperada :
 A nova lista é: 1 2 4 5
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o tamanho da matriz: ");
+        int tamanho = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[tamanho];
+        Console.WriteLine("Insira os elementos na matriz em ordem crescente:");
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.Write($"elemento - {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("Insira a posição para deletar: ");
+        int posicaoDeletar = Convert.ToInt32(Console.ReadLine());
+
+        ExcluirElementoNaPosicao(array, posicaoDeletar);
+
+        Console.WriteLine("\nA nova lista é:");
+        ExibirArray(array);
+    }
+
+    static void ExcluirElementoNaPosicao(int[] array, int posicao)
+    {
+        if (posicao >= 0 && posicao < array.Length)
+        {
+            for (int i = posicao; i < array.Length - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            Array.Resize(ref array, array.Length - 1);
+        }
+        else
+        {
+            Console.WriteLine("Posição inválida.");
+        }
+    }
+
+    static void ExibirArray(int[] array)
+    {
+        foreach (int elemento in array)
+        {
+            Console.Write($"{elemento} ");
+        }
+        Console.WriteLine();
+    }
+}
