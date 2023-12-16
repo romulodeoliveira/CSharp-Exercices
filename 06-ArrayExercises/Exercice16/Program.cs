d77ad002431416983fc1f012cf7c3ad874f410ff
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-15 21:19:11 -0300

    resolução do exercicio 16

--- 06-ArrayExercises/Exercice16/Program.cs
+++ 06-ArrayExercises/Exercice16/Program.cs
@@ -11,3 +11,39 @@ elementos - 4: 6
 Saída esperada :
 O segundo maior elemento em a matriz é: 6
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o tamanho da matriz: ");
+        int tamanho = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[tamanho];
+        Console.WriteLine("Insira os elementos na matriz:");
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.Write($"elemento - {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int segundoMaior = EncontrarSegundoMaior(array);
+
+        Console.WriteLine($"\nO segundo maior elemento na matriz é: {segundoMaior}");
+    }
+
+    static int EncontrarSegundoMaior(int[] array)
+    {
+        if (array.Length < 2)
+        {
+            Console.WriteLine("A matriz deve ter pelo menos dois elementos.");
+            return -1;
+        }
+
+        Array.Sort(array);
+        return array[array.Length - 2];
+    }
+}
