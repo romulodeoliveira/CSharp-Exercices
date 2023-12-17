52cf92e5fb89c6638b19974dccc6fe194eb91f22
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-16 21:23:16 -0300

    resolução do exercicio 17

--- 06-ArrayExercises/Exercice17/Program.cs
+++ 06-ArrayExercises/Exercice17/Program.cs
@@ -11,3 +11,54 @@ elemento - 4: 5
 esperado Saída :
 O segundo menor elemento da matriz é: 4
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o tamanho da matriz: ");
+        int tamanho = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[tamanho];
+        Console.WriteLine("Insira os elementos na matriz (o valor deve ser < 9999):");
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.Write($"elemento - {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int segundoMenor = EncontrarSegundoMenor(array);
+
+        Console.WriteLine($"\nO segundo menor elemento na matriz é: {segundoMenor}");
+    }
+
+    static int EncontrarSegundoMenor(int[] array)
+    {
+        if (array.Length < 2)
+        {
+            Console.WriteLine("A matriz deve ter pelo menos dois elementos.");
+            return -1;
+        }
+
+        int menor = int.MaxValue;
+        int segundoMenor = int.MaxValue;
+
+        foreach (int elemento in array)
+        {
+            if (elemento < menor)
+            {
+                segundoMenor = menor;
+                menor = elemento;
+            }
+            else if (elemento < segundoMenor && elemento != menor)
+            {
+                segundoMenor = elemento;
+            }
+        }
+
+        return segundoMenor;
+    }
+}
