0f5775122dc6bc16161c179e504196984389b510
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-18 21:47:28 -0300

    resolução do exercicio 19

--- 06-ArrayExercises/Exercice19/Program.cs
+++ 06-ArrayExercises/Exercice19/Program.cs
@@ -26,3 +26,77 @@ A adição de duas matrizes é:
 6 8
 10 12
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o tamanho da matriz quadrada (menor que 5): ");
+        int tamanho = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matriz1 = PreencherMatriz("primeira", tamanho);
+
+        int[,] matriz2 = PreencherMatriz("segunda", tamanho);
+
+        int[,] resultado = AdicionarMatrizes(matriz1, matriz2);
+
+        Console.WriteLine("\nA primeira matriz é:\n");
+        ExibirMatriz(matriz1);
+
+        Console.WriteLine("\nA segunda matriz é:\n");
+        ExibirMatriz(matriz2);
+
+        Console.WriteLine("\nA adição de duas matrizes é:\n");
+        ExibirMatriz(resultado);
+    }
+
+    static int[,] PreencherMatriz(string nome, int tamanho)
+    {
+        Console.WriteLine($"\nElementos de entrada na {nome} matriz:");
+
+        int[,] matriz = new int[tamanho, tamanho];
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            for (int j = 0; j < tamanho; j++)
+            {
+                Console.Write($"elemento - [{i}],[{j}] : ");
+                matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        return matriz;
+    }
+
+    static int[,] AdicionarMatrizes(int[,] matriz1, int[,] matriz2)
+    {
+        int tamanho = matriz1.GetLength(0);
+        int[,] resultado = new int[tamanho, tamanho];
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            for (int j = 0; j < tamanho; j++)
+            {
+                resultado[i, j] = matriz1[i, j] + matriz2[i, j];
+            }
+        }
+
+        return resultado;
+    }
+
+    static void ExibirMatriz(int[,] matriz)
+    {
+        int tamanho = matriz.GetLength(0);
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            for (int j = 0; j < tamanho; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
