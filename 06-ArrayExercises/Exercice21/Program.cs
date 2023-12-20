9d62a8efbf6af3a3aeb431d5f53c68687a0c8cf9
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-20 00:02:29 -0300

    resolução do exercicio 21

--- 06-ArrayExercises/Exercice21/Program.cs
+++ 06-ArrayExercises/Exercice21/Program.cs
@@ -27,3 +27,97 @@ A multiplicação de duas matrizes é:
 19 22
 43 50
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira as linhas e colunas da primeira matriz (por exemplo, 2 2): ");
+        string[] dimensoes1 = Console.ReadLine().Split();
+        int linhas1 = Convert.ToInt32(dimensoes1[0]);
+        int colunas1 = Convert.ToInt32(dimensoes1[1]);
+
+        Console.Write("Insira as linhas e colunas da segunda matriz (por exemplo, 2 2): ");
+        string[] dimensoes2 = Console.ReadLine().Split();
+        int linhas2 = Convert.ToInt32(dimensoes2[0]);
+        int colunas2 = Convert.ToInt32(dimensoes2[1]);
+
+        if (colunas1 != linhas2)
+        {
+            Console.WriteLine("A multiplicação não é possível. O número de colunas da primeira matriz deve ser igual ao número de linhas da segunda matriz.");
+            return;
+        }
+
+        int[,] matriz1 = PreencherMatriz("primeira", linhas1, colunas1);
+
+        int[,] matriz2 = PreencherMatriz("segunda", linhas2, colunas2);
+
+        int[,] resultado = MultiplicarMatrizes(matriz1, matriz2);
+
+        Console.WriteLine("\nA primeira matriz é:\n");
+        ExibirMatriz(matriz1);
+
+        Console.WriteLine("\nA segunda matriz é:\n");
+        ExibirMatriz(matriz2);
+
+        Console.WriteLine("\nA multiplicação de duas matrizes é:\n");
+        ExibirMatriz(resultado);
+    }
+
+    static int[,] PreencherMatriz(string nome, int linhas, int colunas)
+    {
+        Console.WriteLine($"\nElementos de entrada na {nome} matriz:");
+
+        int[,] matriz = new int[linhas, colunas];
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write($"elemento - [{i}],[{j}] : ");
+                matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        return matriz;
+    }
+
+    static int[,] MultiplicarMatrizes(int[,] matriz1, int[,] matriz2)
+    {
+        int linhas1 = matriz1.GetLength(0);
+        int colunas1 = matriz1.GetLength(1);
+        int colunas2 = matriz2.GetLength(1);
+
+        int[,] resultado = new int[linhas1, colunas2];
+
+        for (int i = 0; i < linhas1; i++)
+        {
+            for (int j = 0; j < colunas2; j++)
+            {
+                for (int k = 0; k < colunas1; k++)
+                {
+                    resultado[i, j] += matriz1[i, k] * matriz2[k, j];
+                }
+            }
+        }
+
+        return resultado;
+    }
+
+    static void ExibirMatriz(int[,] matriz)
+    {
+        int linhas = matriz.GetLength(0);
+        int colunas = matriz.GetLength(1);
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
