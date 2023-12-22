5de4336ecba557d23337eef83f74af4c51fb4ece
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:05:49 -0300

    resolução do exercicio 70

--- 05-ForLoopExercises/Exercice70/Program.cs
+++ 05-ForLoopExercises/Exercice70/Program.cs
@@ -1,13 +1,44 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como M com um asterisco.
-Exiba o padrão como 'M' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- * *                                                                           
- * *                                                                           
- ** **                                                                           
- * * *                                                                           
- * *                                                                           
- * *                                                                           
+Exiba o padrão como 'M' com um asterisco:
+---------------------------------------------
+
+ * *
+ * *
+ ** **
+ * * *
+ * *
+ * *
  * *
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  DisplayAlphabetPatternM();
+ }
+
+ static void DisplayAlphabetPatternM()
+ {
+  Console.WriteLine("Exibe o padrão como 'M' com um asterisco:");
+
+  for (int i = 1; i <= 7; i++)
+  {
+   for (int j = 1; j <= 6; j++)
+   {
+    if (j == 1 || j == 6 || (i <= 3 && (j == i || j == 7 - i)) || (i > 3 && j == 4 - (i - 3)))
+    {
+     Console.Write("*");
+    }
+    else
+    {
+     Console.Write(" ");
+    }
+   }
+   Console.WriteLine();
+  }
+ }
+}
