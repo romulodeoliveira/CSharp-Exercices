bd1b9fa57ddc0213198772e98386957ce657d708
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:05:41 -0300

    resolução do exercicio 69

--- 05-ForLoopExercises/Exercice69/Program.cs
+++ 05-ForLoopExercises/Exercice69/Program.cs
@@ -1,13 +1,44 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como L com um asterisco.
-Exiba o padrão como 'L' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- *                                                                               
- *                                                                               
- *                                                                               
- *                                                                               
- *                                                                               
- *                                                                               
+Exiba o padrão como 'L' com um asterisco:
+---------------------------------------------
+
+ *
+ *
+ *
+ *
+ *
+ *
  *****
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  DisplayAlphabetPatternL();
+ }
+
+ static void DisplayAlphabetPatternL()
+ {
+  Console.WriteLine("Exibe o padrão como 'L' com um asterisco:");
+
+  for (int i = 1; i <= 7; i++)
+  {
+   for (int j = 1; j <= 5; j++)
+   {
+    if (j == 1 || i == 7)
+    {
+     Console.Write("*");
+    }
+    else
+    {
+     Console.Write(" ");
+    }
+   }
+   Console.WriteLine();
+  }
+ }
+}
