9a0bcb0a93027aaf3033f23d6b63bc408f425ada
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:08:21 -0300

    resolução do exercicio 78

--- 05-ForLoopExercises/Exercice78/Program.cs
+++ 05-ForLoopExercises/Exercice78/Program.cs
@@ -1,15 +1,46 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como U com um asterisco.
 
-Exiba o padrão como 'U' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- * *                                                                           
- * *                                                                           
- * *                                                                           
- * *                                                                           
+Exiba o padrão como 'U' com um asterisco:
+---------------------------------------------
 
- * *                                                                           
- * *                                                                           
+ * *
+ * *
+ * *
+ * *
+
+ * *
+ * *
   ***
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        DisplayAlphabetPatternU();
+    }
+
+    static void DisplayAlphabetPatternU()
+    {
+        Console.WriteLine("Exibe o padrão como 'U' com um asterisco:");
+
+        for (int i = 1; i <= 7; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                if ((j == 1 || j == 5) || (i > 1 && i < 7 && (j == 2 || j == 4)))
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
