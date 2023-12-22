b58fbd3c2e23f4bdb3fe9c7a1b45d9fb9d2e1194
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:08:37 -0300

    resolução do exercicio 81

--- 05-ForLoopExercises/Exercice81/Program.cs
+++ 05-ForLoopExercises/Exercice81/Program.cs
@@ -1,14 +1,45 @@
-﻿/*
-Escreva um programa C# Sharp para exibir um padrão de alfabeto como X com um asterisco.
+﻿ /*
+ Escreva um programa C# Sharp para exibir um padrão de alfabeto como X com um asterisco.
 
-Exiba o padrão como 'X' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- *    *                                                                           
- *    *                                                                           
-  * *                                                                            
-   *                                                                             
-  * *                                                                            
- *   *                                                                           
- *   *
-*/
+ Exiba o padrão como 'X' com um asterisco:                                      
+ ---------------------------------------------                                    
+                                                                                  
+  *    *                                                                           
+  *    *                                                                           
+   * *                                                                            
+    *                                                                             
+   * *                                                                            
+  *   *                                                                           
+  *   *
+ */
+ 
+ using System;
+
+ class Program
+ {
+  static void Main()
+  {
+   DisplayAlphabetPatternX();
+  }
+
+  static void DisplayAlphabetPatternX()
+  {
+   Console.WriteLine("Exibe o padrão como 'X' com um asterisco:");
+
+   for (int i = 1; i <= 7; i++)
+   {
+    for (int j = 1; j <= 7; j++)
+    {
+     if ((i == j || i + j == 8) && (j == 2 || j == 4 || j == 6))
+     {
+      Console.Write("*");
+     }
+     else
+     {
+      Console.Write(" ");
+     }
+    }
+    Console.WriteLine();
+   }
+  }
+ }
