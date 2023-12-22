999649443352cab740dfb061b609f2c635f038d4
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:05:01 -0300

    resolução do exercicio 62

--- 05-ForLoopExercises/Exercice62/Program.cs
+++ 05-ForLoopExercises/Exercice62/Program.cs
@@ -1,14 +1,45 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como E com um asterisco.
 
-Exiba o padrão como 'E' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- *****                                                                           
- *                                                                               
- *                                                                               
- ****                                                                            
- *                                                                               
- *                                                                               
+Exiba o padrão como 'E' com um asterisco:
+---------------------------------------------
+
+ *****
+ *
+ *
+ ****
+ *
+ *
  *****
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  DisplayAlphabetPatternE();
+ }
+
+ static void DisplayAlphabetPatternE()
+ {
+  Console.WriteLine("Exibe o padrão como 'E' com um asterisco:");
+
+  for (int i = 1; i <= 7; i++)
+  {
+   for (int j = 1; j <= 5; j++)
+   {
+    if ((i == 1 || i == 7) || (j == 1) || (j == 5 && i != 1 && i != 7))
+    {
+     Console.Write("*");
+    }
+    else
+    {
+     Console.Write(" ");
+    }
+   }
+   Console.WriteLine();
+  }
+ }
+}
