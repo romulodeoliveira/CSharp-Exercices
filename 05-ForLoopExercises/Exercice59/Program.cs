9b7abe681ad4597242f2df24d9cae243927c1878
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:04:34 -0300

    resolução do exercicio 59

--- 05-ForLoopExercises/Exercice59/Program.cs
+++ 05-ForLoopExercises/Exercice59/Program.cs
@@ -1,14 +1,45 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como B com um asterisco.
 
-Exiba o padrão como 'B' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- ****                                                                            
- * *                                                                           
- * *                                                                           
- ****                                                                            
- * *                                                                           
- * *                                                                           
+Exiba o padrão como 'B' com um asterisco:
+---------------------------------------------
+
+ ****
+ * *
+ * *
+ ****
+ * *
+ * *
  ****
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  DisplayAlphabetPatternB();
+ }
+
+ static void DisplayAlphabetPatternB()
+ {
+  Console.WriteLine("Exibe o padrão como 'B' com um asterisco:");
+
+  for (int i = 1; i <= 7; i++)
+  {
+   for (int j = 1; j <= 4; j++)
+   {
+    if ((i == 1 || i == 7 || i == 4) || (j == 1 && i < 4) || (j == 4 && i > 4))
+    {
+     Console.Write("*");
+    }
+    else
+    {
+     Console.Write(" ");
+    }
+   }
+   Console.WriteLine();
+  }
+ }
+}
