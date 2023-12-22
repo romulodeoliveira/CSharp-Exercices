64aa2e3d3dc790c58cedef62e48971cbcd5968bc
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:08:48 -0300

    resolução do exercicio 83

--- 05-ForLoopExercises/Exercice83/Program.cs
+++ 05-ForLoopExercises/Exercice83/Program.cs
@@ -1,14 +1,45 @@
 ﻿/*
 Escreva um programa C#Sharp para exibir um padrão de alfabeto como Z com um asterisco.
 
-Exiba o padrão como 'Z' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
-*******                                                                          
-     *                                                                           
-    *                                                                            
-   *                                                                             
-  *                                                                              
- *                                                                               
+Exiba o padrão como 'Z' com um asterisco:
+---------------------------------------------
+
+*******
+     *
+    *
+   *
+  *
+ *
 *******
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  DisplayAlphabetPatternZ();
+ }
+
+ static void DisplayAlphabetPatternZ()
+ {
+  Console.WriteLine("Exibe o padrão como 'Z' com um asterisco:");
+
+  for (int i = 1; i <= 7; i++)
+  {
+   for (int j = 1; j <= 7; j++)
+   {
+    if (i == 1 || i == 7 || i + j == 8)
+    {
+     Console.Write("*");
+    }
+    else
+    {
+     Console.Write(" ");
+    }
+   }
+   Console.WriteLine();
+  }
+ }
+}
