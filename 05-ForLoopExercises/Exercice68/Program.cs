d6a9d107cdc51bab87cacfcdda7bf1eba9e9f06d
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:05:36 -0300

    resolução do exercicio 68

--- 05-ForLoopExercises/Exercice68/Program.cs
+++ 05-ForLoopExercises/Exercice68/Program.cs
@@ -1,13 +1,44 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como K com um asterisco.
-Exiba o padrão como 'K' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- * *                                                                           
- * *                                                                            
- * *                                                                             
- **                                                                              
- * *                                                                             
- * *                                                                            
+Exiba o padrão como 'K' com um asterisco:
+---------------------------------------------
+
+ * *
+ * *
+ * *
+ **
+ * *
+ * *
  * *
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  DisplayAlphabetPatternK();
+ }
+
+ static void DisplayAlphabetPatternK()
+ {
+  Console.WriteLine("Exibe o padrão como 'K' com um asterisco:");
+
+  for (int i = 1; i <= 7; i++)
+  {
+   for (int j = 1; j <= 5; j++)
+   {
+    if (j == 1 || (i > 1 && i <= 4 && j == 5 - i) || (i >= 4 && j == i - 3) || j == 5)
+    {
+     Console.Write("*");
+    }
+    else
+    {
+     Console.Write(" ");
+    }
+   }
+   Console.WriteLine();
+  }
+ }
+}
