e9c4c63a414c0e020295fbb50d82a7d4cf309da4
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:08:18 -0300

    resolução do exercicio 77

--- 05-ForLoopExercises/Exercice77/Program.cs
+++ 05-ForLoopExercises/Exercice77/Program.cs
@@ -1,14 +1,45 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como T com um asterisco.
 
-Exiba o padrão como 'T' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- *****                                                                           
-   *                                                                             
-   *                                                                             
-   *                                                                             
-   *                                                                             
-   *                                                                             
+Exiba o padrão como 'T' com um asterisco:
+---------------------------------------------
+
+ *****
+   *
+   *
+   *
+   *
+   *
    *
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        DisplayAlphabetPatternT();
+    }
+
+    static void DisplayAlphabetPatternT()
+    {
+        Console.WriteLine("Exibe o padrão como 'T' com um asterisco:");
+
+        for (int i = 1; i <= 7; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                if (i == 1 || j == 3)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
