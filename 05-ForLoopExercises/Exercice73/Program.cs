fa09eb58e3ec695576cf35359e47ab4d92b16de6
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:06:01 -0300

    resolução do exercicio 73

--- 05-ForLoopExercises/Exercice73/Program.cs
+++ 05-ForLoopExercises/Exercice73/Program.cs
@@ -1,14 +1,45 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como P com um asterisco.
 
-Exiba o padrão como 'P' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- ****                                                                            
- * *                                                                           
- * *                                                                           
- ****                                                                            
- *                                                                               
- *                                                                               
+Exiba o padrão como 'P' com um asterisco:
+---------------------------------------------
+
+ ****
+ * *
+ * *
+ ****
+ *
+ *
  *
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  DisplayAlphabetPatternP();
+ }
+
+ static void DisplayAlphabetPatternP()
+ {
+  Console.WriteLine("Exibe o padrão como 'P' com um asterisco:");
+
+  for (int i = 1; i <= 7; i++)
+  {
+   for (int j = 1; j <= 5; j++)
+   {
+    if ((j == 1 || j == 5) || ((i == 1 || i == 4) && j < 5) || (i > 1 && i < 4 && j == 5))
+    {
+     Console.Write("*");
+    }
+    else
+    {
+     Console.Write(" ");
+    }
+   }
+   Console.WriteLine();
+  }
+ }
+}
