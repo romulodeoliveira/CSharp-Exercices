87551bebbf3d3372cb4f82fac9a1851792f3cff4
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:08:44 -0300

    resolução do exercicio 82

--- 05-ForLoopExercises/Exercice82/Program.cs
+++ 05-ForLoopExercises/Exercice82/Program.cs
@@ -1,14 +1,45 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como Y com um asterisco.
 
-Exiba o padrão como 'Y' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- *       *                                                                           
-  *     *                                                                           
-    * *                                                                            
-     *                                                                             
-     *                                                                             
-     *                                                                             
+Exiba o padrão como 'Y' com um asterisco:
+---------------------------------------------
+
+ *       *
+  *     *
+    * *
+     *
+     *
+     *
      *
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        DisplayAlphabetPatternY();
+    }
+
+    static void DisplayAlphabetPatternY()
+    {
+        Console.WriteLine("Exibe o padrão como 'Y' com um asterisco:");
+
+        for (int i = 1; i <= 7; i++)
+        {
+            for (int j = 1; j <= 7; j++)
+            {
+                if ((j == 1 || j == 7) && (i <= 4) || (i == j) && (j > 1 && j < 4) || (i + j == 8) && (j > 4 && j < 7))
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
