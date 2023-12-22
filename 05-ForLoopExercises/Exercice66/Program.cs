a619a5e7a319eb9c3ea9a8e95af20444c0e6fb5e
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:05:26 -0300

    resolução do exercicio 66

--- 05-ForLoopExercises/Exercice66/Program.cs
+++ 05-ForLoopExercises/Exercice66/Program.cs
@@ -1,14 +1,45 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como I com um asterisco.
 
-Exiba o padrão como 'I' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- *****                                                                           
-   *                                                                             
-   *                                                                             
-   *                                                                             
-   *                                                                             
-   *                                                                             
+Exiba o padrão como 'I' com um asterisco:
+---------------------------------------------
+
+ *****
+   *
+   *
+   *
+   *
+   *
  *****
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        DisplayAlphabetPatternI();
+    }
+
+    static void DisplayAlphabetPatternI()
+    {
+        Console.WriteLine("Exibe o padrão como 'I' com um asterisco:");
+
+        for (int i = 1; i <= 7; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                if ((i == 1 || i == 7) || (j == 3 && i != 1 && i != 7))
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
