0c59c96f01a5b3d07b17facb22068b1ef2c025e1
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:05:09 -0300

    resolução do exercicio 63

--- 05-ForLoopExercises/Exercice63/Program.cs
+++ 05-ForLoopExercises/Exercice63/Program.cs
@@ -1,14 +1,45 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como F com um asterisco.
 
-Exiba o padrão como 'F' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- *****                                                                           
- *                                                                               
- *                                                                               
- ****                                                                            
- *                                                                               
- *                                                                               
+Exiba o padrão como 'F' com um asterisco:
+---------------------------------------------
+
+ *****
+ *
+ *
+ ****
+ *
+ *
  *
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  DisplayAlphabetPatternF();
+ }
+
+ static void DisplayAlphabetPatternF()
+ {
+  Console.WriteLine("Exibe o padrão como 'F' com um asterisco:");
+
+  for (int i = 1; i <= 7; i++)
+  {
+   for (int j = 1; j <= 5; j++)
+   {
+    if ((i == 1) || (j == 1) || (j == 5 && i <= 4))
+    {
+     Console.Write("*");
+    }
+    else
+    {
+     Console.Write(" ");
+    }
+   }
+   Console.WriteLine();
+  }
+ }
+}
