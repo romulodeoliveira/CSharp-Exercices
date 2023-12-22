fc47c91b86f1378605daee785a2d8a87a03325ea
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:05:58 -0300

    resolução do exercicio 72

--- 05-ForLoopExercises/Exercice72/Program.cs
+++ 05-ForLoopExercises/Exercice72/Program.cs
@@ -1,14 +1,49 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como um O com um asterisco.
 
-Exiba o padrão como 'O' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
-  ***                                                                            
- * *                                                                           
- * *                                                                           
- * *                                                                           
- * *                                                                           
- * *                                                                           
+Exiba o padrão como 'O' com um asterisco:
+---------------------------------------------
+
+  ***
+ * *
+ * *
+ * *
+ * *
+ * *
   ***
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        DisplayAlphabetPatternO();
+    }
+
+    static void DisplayAlphabetPatternO()
+    {
+        Console.WriteLine("Exibe o padrão como 'O' com um asterisco:");
+
+        for (int i = 1; i <= 7; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                if ((i == 1 || i == 7) && (j == 2 || j == 3 || j == 4))
+                {
+                    Console.Write("*");
+                }
+                else if ((i > 1 && i < 7) && (j == 1 || j == 5))
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
