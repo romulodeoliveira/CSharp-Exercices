8c0310cecefbcede30c2596a837cd7415f2c2120
Romulo de Oliveira <dev@romulodeoliveira.net>
2023-12-22 16:05:54 -0300

    resolução do exercicio 71

--- 05-ForLoopExercises/Exercice71/Program.cs
+++ 05-ForLoopExercises/Exercice71/Program.cs
@@ -1,14 +1,45 @@
 ﻿/*
 Escreva um programa C# Sharp para exibir um padrão de alfabeto como N com um asterisco.
 
-Exiba o padrão como 'N' com um asterisco:                                      
----------------------------------------------                                    
-                                                                                 
- * *                                                                           
- * *                                                                           
- ** *                                                                           
- * * *                                                                           
- * **                                                                           
- * *                                                                           
+Exiba o padrão como 'N' com um asterisco:
+---------------------------------------------
+
+ * *
+ * *
+ ** *
+ * * *
+ * **
+ * *
  * *
 */
+
+using System;
+
+class Program
+{
+ static void Main()
+ {
+  DisplayAlphabetPatternN();
+ }
+
+ static void DisplayAlphabetPatternN()
+ {
+  Console.WriteLine("Exibe o padrão como 'N' com um asterisco:");
+
+  for (int i = 1; i <= 7; i++)
+  {
+   for (int j = 1; j <= 5; j++)
+   {
+    if (j == 1 || j == 5 || (i > 1 && i < 6 && j == i) || (i > 1 && i < 6 && j == 6 - i))
+    {
+     Console.Write("*");
+    }
+    else
+    {
+     Console.Write(" ");
+    }
+   }
+   Console.WriteLine();
+  }
+ }
+}
