70614f96d6227632d8768ce142157db378e8767e
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-12-23 23:19:55 -0300

    resolução do exercicio 23

--- 06-ArrayExercises/Exercice23/Program.cs
+++ 06-ArrayExercises/Exercice23/Program.cs
@@ -14,3 +14,69 @@ A matriz é :
 Adição dos elementos diagonais corretos é :5
 Elementos na matriz são:
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o tamanho da matriz quadrada: ");
+        int tamanho = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matriz = PreencherMatriz(tamanho);
+
+        Console.WriteLine("\nA matriz é:\n");
+        ExibirMatriz(matriz);
+
+        int somaDiagonalDireita = SomaDiagonalDireita(matriz);
+
+        Console.WriteLine($"\nAdição dos elementos diagonais corretos é: {somaDiagonalDireita}");
+    }
+
+    static int[,] PreencherMatriz(int tamanho)
+    {
+        Console.WriteLine($"\nElementos na matriz ({tamanho}x{tamanho}):");
+
+        int[,] matriz = new int[tamanho, tamanho];
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            for (int j = 0; j < tamanho; j++)
+            {
+                Console.Write($"elemento - [{i}],[{j}] : ");
+                matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        return matriz;
+    }
+
+    static int SomaDiagonalDireita(int[,] matriz)
+    {
+        int tamanho = matriz.GetLength(0);
+        int soma = 0;
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            soma += matriz[i, i];
+        }
+
+        return soma;
+    }
+
+    static void ExibirMatriz(int[,] matriz)
+    {
+        int linhas = matriz.GetLength(0);
+        int colunas = matriz.GetLength(1);
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
