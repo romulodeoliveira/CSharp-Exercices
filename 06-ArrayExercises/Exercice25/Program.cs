aa17cb09665d25cab01d331d52d60d1d417bfd32
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-12-25 11:54:21 -0300

    resolução do exercicio 25

--- 06-ArrayExercises/Exercice25/Program.cs
+++ 06-ArrayExercises/Exercice25/Program.cs
@@ -18,3 +18,102 @@ A soma ou linhas e colunas da matriz é :
 
 12 14
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o tamanho da matriz quadrada: ");
+        int tamanho = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matriz = PreencherMatriz(tamanho);
+
+        Console.WriteLine("\nA matriz é:\n");
+        ExibirMatriz(matriz);
+
+        int[] somaLinhas = SomaLinhas(matriz);
+        int[] somaColunas = SomaColunas(matriz);
+
+        Console.WriteLine("\nA soma das linhas da matriz é:");
+        ExibirArray(somaLinhas);
+
+        Console.WriteLine("\nA soma das colunas da matriz é:");
+        ExibirArray(somaColunas);
+    }
+
+    static int[,] PreencherMatriz(int tamanho)
+    {
+        Console.WriteLine($"\nElementos na matriz ({tamanho}x{tamanho}):");
+
+        int[,] matriz = new int[tamanho, tamanho];
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            for (int j = 0; j < tamanho; j++)
+            {
+                Console.Write($"elemento - [{i}],[{j}] : ");
+                matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        return matriz;
+    }
+
+    static int[] SomaLinhas(int[,] matriz)
+    {
+        int tamanho = matriz.GetLength(0);
+        int[] somaLinhas = new int[tamanho];
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            for (int j = 0; j < tamanho; j++)
+            {
+                somaLinhas[i] += matriz[i, j];
+            }
+        }
+
+        return somaLinhas;
+    }
+
+    static int[] SomaColunas(int[,] matriz)
+    {
+        int tamanho = matriz.GetLength(1);
+        int[] somaColunas = new int[tamanho];
+
+        for (int j = 0; j < tamanho; j++)
+        {
+            for (int i = 0; i < tamanho; i++)
+            {
+                somaColunas[j] += matriz[i, j];
+            }
+        }
+
+        return somaColunas;
+    }
+
+    static void ExibirMatriz(int[,] matriz)
+    {
+        int linhas = matriz.GetLength(0);
+        int colunas = matriz.GetLength(1);
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void ExibirArray(int[] array)
+    {
+        foreach (int elemento in array)
+        {
+            Console.Write(elemento + " ");
+        }
+        Console.WriteLine();
+    }
+}
